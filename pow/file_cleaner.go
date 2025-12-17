5750f28bc896576eebf55b012aec688d57429e32
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-17 15:57:56 +0000

    Add utility to clean up temporary files older than 7 days

---
+++ pow/file_cleaner.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	maxAgeHours  = 168
+)
+
+func main() {
+	err := cleanOldFiles(tempDir, maxAgeHours)
+	if err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string, maxAgeHours int) error {
+	cutoffTime := time.Now().Add(-time.Duration(maxAgeHours) * time.Hour)
+
+	return filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing old file: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
+		}
+		return nil
+	})
+}
