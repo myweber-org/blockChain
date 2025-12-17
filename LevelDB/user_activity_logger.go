bc718495ee02703baa2e7a30ab6df12f8f18e767
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-17 23:14:15 +0000

    Add user activity logging middleware for API endpoints

---
+++ LevelDB/user_activity_logger.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ip := r.RemoteAddr
+	method := r.Method
+	path := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from %s %s %s completed in %v", userID, ip, method, path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if auth := r.Header.Get("Authorization"); auth != "" {
+		return "authenticated_user"
+	}
+	return "anonymous"
+}
