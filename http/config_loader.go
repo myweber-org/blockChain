cc14ff1bf181d5b3e2312ae43327640d9dc91215
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-18 18:15:37 +0000

    Add configuration loader with YAML parsing and validation support

--- http/config_loader.go
+++ http/config_loader.go
@@ -53,4 +53,74 @@ func validateConfig(config *ServerConfig) error {
     }
 
     return nil
+}package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Host     string `yaml:"host"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	if path == "" {
+		return nil, errors.New("config path cannot be empty")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	if config.Server.Host == "" {
+		config.Server.Host = "localhost"
+	}
+	if config.Server.Port == 0 {
+		config.Server.Port = 8080
+	}
+	if config.LogLevel == "" {
+		config.LogLevel = "info"
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *Config) error {
+	if config.Server.Port < 1 || config.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if config.Database.Host == "" {
+		return errors.New("database host is required")
+	}
+	if config.Database.Name == "" {
+		return errors.New("database name is required")
+	}
+	return nil
 }
