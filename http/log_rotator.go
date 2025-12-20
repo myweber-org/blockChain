7f897f447327eacccb756c9a54e1cc851d2951f5
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-20 15:14:57 +0000

    Implement a basic log rotation mechanism with size-based file rotation

--- http/log_rotator.go
+++ http/log_rotator.go
@@ -137,4 +137,94 @@ func main() {
         rotator.Write([]byte(message))
         time.Sleep(10 * time.Millisecond)
     }
+}package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type RotatingWriter struct {
+	mu          sync.Mutex
+	current     *os.File
+	maxSize     int64
+	basePath    string
+	currentSize int64
+	fileIndex   int
+}
+
+func NewRotatingWriter(basePath string, maxSize int64) (*RotatingWriter, error) {
+	w := &RotatingWriter{
+		maxSize:  maxSize,
+		basePath: basePath,
+	}
+	if err := w.rotate(); err != nil {
+		return nil, err
+	}
+	return w, nil
+}
+
+func (w *RotatingWriter) Write(p []byte) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.currentSize+int64(len(p)) > w.maxSize {
+		if err := w.rotate(); err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := w.current.Write(p)
+	if err == nil {
+		w.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (w *RotatingWriter) rotate() error {
+	if w.current != nil {
+		w.current.Close()
+	}
+
+	filename := fmt.Sprintf("%s.%d", w.basePath, w.fileIndex)
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+
+	w.current = file
+	w.currentSize = 0
+	w.fileIndex++
+	return nil
+}
+
+func (w *RotatingWriter) Close() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.current != nil {
+		return w.current.Close()
+	}
+	return nil
+}
+
+func main() {
+	writer, err := NewRotatingWriter("app.log", 1024*10)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create writer: %v\n", err)
+		os.Exit(1)
+	}
+	defer writer.Close()
+
+	for i := 0; i < 100; i++ {
+		line := fmt.Sprintf("Log entry number %d: Some sample log data here.\n", i)
+		if _, err := writer.Write([]byte(line)); err != nil {
+			fmt.Fprintf(os.Stderr, "Write failed: %v\n", err)
+			break
+		}
+	}
+
+	fmt.Println("Log rotation test completed. Check app.log.* files.")
 }
