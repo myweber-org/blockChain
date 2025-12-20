f6b1e4fde444e8d12d78de89ce3eeafae61e0ee8
OaklandAvenue <my.oakland.avenue@outlook.com>
2025-12-20 16:47:10 +0000

    Add log rotation with size-based file splitting and cleanup of old logs

--- block/log_rotator.go
+++ block/log_rotator.go
@@ -613,4 +613,125 @@ func main() {
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
+}package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sort"
+    "strings"
+    "time"
+)
+
+const (
+    maxFileSize  = 10 * 1024 * 1024 // 10MB
+    maxBackups   = 5
+    logExtension = ".log"
+)
+
+type RotatingWriter struct {
+    currentFile *os.File
+    basePath    string
+    fileSize    int64
+}
+
+func NewRotatingWriter(basePath string) (*RotatingWriter, error) {
+    w := &RotatingWriter{basePath: basePath}
+    if err := w.openCurrentFile(); err != nil {
+        return nil, err
+    }
+    return w, nil
+}
+
+func (w *RotatingWriter) Write(p []byte) (int, error) {
+    if w.fileSize+int64(len(p)) > maxFileSize {
+        if err := w.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := w.currentFile.Write(p)
+    w.fileSize += int64(n)
+    return n, err
+}
+
+func (w *RotatingWriter) rotate() error {
+    if w.currentFile != nil {
+        w.currentFile.Close()
+    }
+
+    timestamp := time.Now().Format("20060102_150405")
+    newPath := fmt.Sprintf("%s_%s%s", w.basePath, timestamp, logExtension)
+    if err := os.Rename(w.basePath+logExtension, newPath); err != nil {
+        return err
+    }
+
+    if err := w.cleanupOldFiles(); err != nil {
+        return err
+    }
+
+    return w.openCurrentFile()
+}
+
+func (w *RotatingWriter) openCurrentFile() error {
+    file, err := os.OpenFile(w.basePath+logExtension, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        return err
+    }
+
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return err
+    }
+
+    w.currentFile = file
+    w.fileSize = info.Size()
+    return nil
+}
+
+func (w *RotatingWriter) cleanupOldFiles() error {
+    pattern := fmt.Sprintf("%s_*%s", w.basePath, logExtension)
+    matches, err := filepath.Glob(pattern)
+    if err != nil {
+        return err
+    }
+
+    sort.Sort(sort.Reverse(sort.StringSlice(matches)))
+
+    for i, match := range matches {
+        if i >= maxBackups {
+            os.Remove(match)
+        }
+    }
+    return nil
+}
+
+func (w *RotatingWriter) Close() error {
+    if w.currentFile != nil {
+        return w.currentFile.Close()
+    }
+    return nil
+}
+
+func main() {
+    writer, err := NewRotatingWriter("application")
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to create writer: %v\n", err)
+        os.Exit(1)
+    }
+    defer writer.Close()
+
+    for i := 0; i < 1000; i++ {
+        logLine := fmt.Sprintf("[%s] Log entry number %d\n", time.Now().Format(time.RFC3339), i)
+        if _, err := writer.Write([]byte(logLine)); err != nil {
+            fmt.Fprintf(os.Stderr, "Write failed: %v\n", err)
+            break
+        }
+        time.Sleep(10 * time.Millisecond)
+    }
+
+    fmt.Println("Log rotation test completed")
 }
