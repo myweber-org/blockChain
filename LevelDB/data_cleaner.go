9b3591a88da1c01d09f6c3cb5738b1c55899358e
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-23 20:48:11 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- LevelDB/data_cleaner.go
+++ LevelDB/data_cleaner.go
@@ -63,4 +63,18 @@ func main() {
     }
 
     fmt.Printf("Data cleaned successfully. Output saved to %s\n", outputFile)
+}package datautil
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
 }
