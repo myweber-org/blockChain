f0191f7b224054f583132ca4ec64fbd7dc775333
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-23 09:06:20 +0000

    Add user activity logging with timestamps and event types

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -197,4 +197,76 @@ func ActivityLogger(next http.Handler) http.Handler {
 			duration,
 		)
 	})
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityEvent struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	EventType string    `json:"event_type"`
+	Details   string    `json:"details"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+	encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{
+		logFile: file,
+		encoder: json.NewEncoder(file),
+	}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+	event := ActivityEvent{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		EventType: eventType,
+		Details:   details,
+	}
+	return l.encoder.Encode(event)
+}
+
+func (l *ActivityLogger) Close() error {
+	return l.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	events := []struct {
+		userID    string
+		eventType string
+		details   string
+	}{
+		{"user123", "login", "Successful authentication"},
+		{"user123", "view_page", "Accessed dashboard"},
+		{"user456", "purchase", "Order ID: ORD-78910"},
+		{"user123", "logout", "Session terminated"},
+	}
+
+	for _, e := range events {
+		if err := logger.LogActivity(e.userID, e.eventType, e.details); err != nil {
+			log.Printf("Failed to log activity: %v", err)
+		}
+	}
+
+	fmt.Println("Activity logging completed")
 }
