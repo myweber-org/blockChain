25a4fe64847dd27774aed7874dce5faf666cd56c
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-24 19:56:51 +0000

    Add file encryption utility with AES-GCM and password-based key derivation

--- pow/file_encryptor.go
+++ pow/file_encryptor.go
@@ -220,3 +220,143 @@ func main() {
 	os.Remove("test.enc")
 	os.Remove("test.dec")
 }
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func deriveKey(password string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptData(plaintext []byte, password string) (string, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	key := deriveKey(password, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	result := append(salt, ciphertext...)
+	return base64.StdEncoding.EncodeToString(result), nil
+}
+
+func decryptData(encrypted string, password string) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < 16 {
+		return nil, errors.New("invalid encrypted data")
+	}
+
+	salt := data[:16]
+	ciphertext := data[16:]
+	key := deriveKey(password, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_encryptor.go <encrypt|decrypt> <input_file> <password> [output_file]")
+		os.Exit(1)
+	}
+
+	action := strings.ToLower(os.Args[1])
+	inputFile := os.Args[2]
+	password := os.Args[3]
+
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	switch action {
+	case "encrypt":
+		encrypted, err := encryptData(data, password)
+		if err != nil {
+			fmt.Printf("Encryption error: %v\n", err)
+			os.Exit(1)
+		}
+
+		outputFile := "encrypted.txt"
+		if len(os.Args) > 4 {
+			outputFile = os.Args[4]
+		}
+
+		if err := os.WriteFile(outputFile, []byte(encrypted), 0644); err != nil {
+			fmt.Printf("Error writing file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("File encrypted successfully: %s\n", outputFile)
+
+	case "decrypt":
+		decrypted, err := decryptData(string(data), password)
+		if err != nil {
+			fmt.Printf("Decryption error: %v\n", err)
+			os.Exit(1)
+		}
+
+		outputFile := "decrypted.txt"
+		if len(os.Args) > 4 {
+			outputFile = os.Args[4]
+		}
+
+		if err := os.WriteFile(outputFile, decrypted, 0644); err != nil {
+			fmt.Printf("Error writing file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("File decrypted successfully: %s\n", outputFile)
+
+	default:
+		fmt.Println("Invalid action. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+}
