75a8dd4710850c3df5392d319affe2c6f4c7fc3e
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-26 19:53:04 +0000

    Add configuration loader with environment variable support

--- block/config_loader.go
+++ block/config_loader.go
@@ -52,4 +52,70 @@ func SaveConfig(config *AppConfig, configPath string) error {
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
 	return encoder.Encode(config)
+}package config
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+)
+
+type Config struct {
+	ServerPort string `json:"server_port"`
+	DBHost     string `json:"db_host"`
+	DBPort     string `json:"db_port"`
+	LogLevel   string `json:"log_level"`
+}
+
+var (
+	config     *Config
+	configOnce sync.Once
+)
+
+func Load() *Config {
+	configOnce.Do(func() {
+		config = &Config{
+			ServerPort: getEnv("SERVER_PORT", "8080"),
+			DBHost:     getEnv("DB_HOST", "localhost"),
+			DBPort:     getEnv("DB_PORT", "5432"),
+			LogLevel:   getEnv("LOG_LEVEL", "info"),
+		}
+
+		if configFile := os.Getenv("CONFIG_FILE"); configFile != "" {
+			loadFromFile(configFile)
+		}
+	})
+	return config
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func loadFromFile(filename string) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	var fileConfig Config
+	if err := json.Unmarshal(data, &fileConfig); err != nil {
+		return
+	}
+
+	if fileConfig.ServerPort != "" {
+		config.ServerPort = fileConfig.ServerPort
+	}
+	if fileConfig.DBHost != "" {
+		config.DBHost = fileConfig.DBHost
+	}
+	if fileConfig.DBPort != "" {
+		config.DBPort = fileConfig.DBPort
+	}
+	if fileConfig.LogLevel != "" {
+		config.LogLevel = fileConfig.LogLevel
+	}
 }
