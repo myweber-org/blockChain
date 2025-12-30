dfd40f682293a66c8eaabd94a71e5a504d63b6d9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-30 00:10:12 +0000

    Add data processing utility for cleaning and validating input strings

--- http/service/data_processor.go
+++ http/service/data_processor.go
@@ -224,3 +224,45 @@ func main() {
     fmt.Println("Processed user profile:")
     fmt.Println(result)
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+	emailRegex      *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+		emailRegex:      regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) ExtractDomain(email string) string {
+	if !dp.ValidateEmail(email) {
+		return ""
+	}
+	parts := strings.Split(email, "@")
+	if len(parts) != 2 {
+		return ""
+	}
+	return parts[1]
+}
+
+func (dp *DataProcessor) NormalizeSpaces(input string) string {
+	return dp.whitespaceRegex.ReplaceAllString(input, " ")
+}
