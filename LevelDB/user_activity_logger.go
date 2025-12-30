09d95a8882003b6d903699ebd5b5a9ff91c10e40
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-30 00:45:33 +0000

    Add user activity logging with timestamps and event types

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -87,4 +87,78 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityEvent struct {
+    UserID    string    `json:"user_id"`
+    EventType string    `json:"event_type"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, eventType, details string) error {
+    event := ActivityEvent{
+        UserID:    userID,
+        EventType: eventType,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.Marshal(event)
+    if err != nil {
+        return err
+    }
+
+    data = append(data, '\n')
+    _, err = al.logFile.Write(data)
+    return err
+}
+
+func (al *ActivityLogger) Close() error {
+    return al.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activity.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "User logged in from IP 192.168.1.100")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = logger.LogActivity("user123", "view_page", "Viewed product page for item ABC-123")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = logger.LogActivity("user456", "purchase", "Purchased item XYZ-789 for $29.99")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Activity logging completed successfully")
 }
