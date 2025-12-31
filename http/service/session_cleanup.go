2d8cb1165abce913e84d0c70a61bb8705653873c
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-31 20:16:56 +0000

    Add session cleanup cron job to remove expired sessions daily

--- http/service/session_cleanup.go
+++ http/service/session_cleanup.go
@@ -1,87 +1,42 @@
+
 package main
 
 import (
-    "context"
-    "log"
-    "time"
+	"context"
+	"log"
+	"time"
 
-    "github.com/jmoiron/sqlx"
-    _ "github.com/lib/pq"
+	"yourproject/internal/database"
+	"yourproject/internal/models"
 )
 
-const (
-    cleanupInterval = 1 * time.Hour
-    sessionTTL      = 24 * time.Hour
-    deleteBatchSize = 1000
-)
+func cleanupExpiredSessions() error {
+	db := database.GetDB()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-type SessionCleaner struct {
-    db *sqlx.DB
-}
+	threshold := time.Now().Add(-24 * time.Hour)
+	result := db.WithContext(ctx).
+		Where("last_activity < ?", threshold).
+		Delete(&models.Session{})
 
-func NewSessionCleaner(db *sqlx.DB) *SessionCleaner {
-    return &SessionCleaner{db: db}
-}
+	if result.Error != nil {
+		return result.Error
+	}
 
-func (sc *SessionCleaner) Run(ctx context.Context) {
-    ticker := time.NewTicker(cleanupInterval)
-    defer ticker.Stop()
-
-    for {
-        select {
-        case <-ctx.Done():
-            log.Println("Session cleanup stopped")
-            return
-        case <-ticker.C:
-            sc.cleanupExpiredSessions()
-        }
-    }
-}
-
-func (sc *SessionCleaner) cleanupExpiredSessions() {
-    cutoffTime := time.Now().Add(-sessionTTL)
-    deletedCount := 0
-
-    for {
-        result, err := sc.db.Exec(`
-            DELETE FROM user_sessions 
-            WHERE last_activity < $1 
-            AND id IN (
-                SELECT id FROM user_sessions 
-                WHERE last_activity < $1 
-                LIMIT $2
-            )`,
-            cutoffTime, deleteBatchSize,
-        )
-
-        if err != nil {
-            log.Printf("Error deleting expired sessions: %v", err)
-            return
-        }
-
-        rowsAffected, _ := result.RowsAffected()
-        deletedCount += int(rowsAffected)
-
-        if rowsAffected < deleteBatchSize {
-            break
-        }
-
-        time.Sleep(100 * time.Millisecond)
-    }
-
-    if deletedCount > 0 {
-        log.Printf("Cleaned up %d expired sessions", deletedCount)
-    }
+	if result.RowsAffected > 0 {
+		log.Printf("Cleaned up %d expired sessions", result.RowsAffected)
+	}
+	return nil
 }
 
 func main() {
-    db, err := sqlx.Connect("postgres", "user=postgres dbname=appdb sslmode=disable")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
 
-    cleaner := NewSessionCleaner(db)
-    ctx := context.Background()
-    cleaner.Run(ctx)
+	for range ticker.C {
+		if err := cleanupExpiredSessions(); err != nil {
+			log.Printf("Session cleanup failed: %v", err)
+		}
+	}
 }
