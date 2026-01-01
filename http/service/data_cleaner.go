d2e4becec2f234412a0bcff4b71b7cc1fee6a12c
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-01 00:23:24 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- http/service/data_cleaner.go
+++ http/service/data_cleaner.go
@@ -1,15 +1,24 @@
-package datautils
 
-func RemoveDuplicates[T comparable](slice []T) []T {
-	seen := make(map[T]bool)
-	result := []T{}
+package main
 
-	for _, item := range slice {
-		if !seen[item] {
-			seen[item] = true
-			result = append(result, item)
+import "fmt"
+
+func removeDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
 		}
 	}
-
 	return result
+}
+
+func main() {
+	data := []int{4, 2, 8, 2, 4, 9, 8, 1}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
 }
