89b3eb3f1b7b0dd0171328798a51a0420b551db0
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-03 20:56:16 +0000

    Add user activity logging with timestamps and event types

--- http/service/user_activity_logger.go
+++ http/service/user_activity_logger.go
@@ -1,67 +1,73 @@
-package middleware
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-	"net/http"
+	"os"
 	"time"
 )
 
+type ActivityEvent struct {
+	UserID    string    `json:"user_id"`
+	EventType string    `json:"event_type"`
+	Timestamp time.Time `json:"timestamp"`
+	Details   string    `json:"details,omitempty"`
+}
+
 type ActivityLogger struct {
-	handler http.Handler
+	logFile *os.File
+	encoder *json.Encoder
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
 	}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
-	duration := time.Since(start)
-	log.Printf("%s %s %d %v", r.Method, r.URL.Path, recorder.statusCode, duration)
+	return &ActivityLogger{
+		logFile: file,
+		encoder: json.NewEncoder(file),
+	}, nil
 }
 
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+	event := ActivityEvent{
+		UserID:    userID,
+		EventType: eventType,
+		Timestamp: time.Now().UTC(),
+		Details:   details,
+	}
+	return l.encoder.Encode(event)
 }
 
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
+func (l *ActivityLogger) Close() error {
+	return l.logFile.Close()
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+	events := []struct {
+		userID    string
+		eventType string
+		details   string
+	}{
+		{"user_001", "login", "Successful authentication"},
+		{"user_001", "view_page", "/dashboard"},
+		{"user_002", "login", "Failed attempt"},
+		{"user_001", "logout", "Session ended"},
+	}
+
+	for _, e := range events {
+		if err := logger.LogActivity(e.userID, e.eventType, e.details); err != nil {
+			fmt.Printf("Failed to log activity: %v\n", err)
+		}
+	}
+
+	fmt.Println("Activity logging completed")
 }
