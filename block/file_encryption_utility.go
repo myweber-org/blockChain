58ddabf949d2ab48d3aed814cd23c3e23a3ae63d
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-03 01:57:59 +0000

    Add file encryption utility with AES-GCM and key derivation

--- block/file_encryption_utility.go
+++ block/file_encryption_utility.go
@@ -4,13 +4,28 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
+	"os"
 )
 
-func encrypt(plaintext []byte, key []byte) (string, error) {
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encrypt(plaintext []byte, passphrase string) (string, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	key := deriveKey(passphrase, salt)
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
@@ -22,20 +37,29 @@ func encrypt(plaintext []byte, key []byte) (string, error) {
 	}
 
 	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+	if _, err := rand.Read(nonce); err != nil {
 		return "", err
 	}
 
 	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
-	return base64.StdEncoding.EncodeToString(ciphertext), nil
+	result := append(salt, ciphertext...)
+	return base64.StdEncoding.EncodeToString(result), nil
 }
 
-func decrypt(ciphertext string, key []byte) ([]byte, error) {
-	data, err := base64.StdEncoding.DecodeString(ciphertext)
+func decrypt(encodedCiphertext string, passphrase string) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(encodedCiphertext)
 	if err != nil {
 		return nil, err
 	}
 
+	if len(data) < 16 {
+		return nil, errors.New("invalid ciphertext length")
+	}
+
+	salt := data[:16]
+	ciphertext := data[16:]
+
+	key := deriveKey(passphrase, salt)
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -47,32 +71,62 @@ func decrypt(ciphertext string, key []byte) ([]byte, error) {
 	}
 
 	nonceSize := gcm.NonceSize()
-	if len(data) < nonceSize {
+	if len(ciphertext) < nonceSize {
 		return nil, errors.New("ciphertext too short")
 	}
 
-	nonce, ciphertextBytes := data[:nonceSize], data[nonceSize:]
-	return gcm.Open(nil, nonce, ciphertextBytes, nil)
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
 }
 
 func main() {
-	key := make([]byte, 32)
-	if _, err := io.ReadFull(rand.Reader, key); err != nil {
-		panic(err)
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_encryption_utility.go <encrypt|decrypt> <input_file> <output_file>")
+		fmt.Println("Passphrase will be read from environment variable ENCRYPTION_KEY")
+		os.Exit(1)
 	}
 
-	message := "Sensitive data requiring encryption"
-	encrypted, err := encrypt([]byte(message), key)
+	operation := os.Args[1]
+	inputFile := os.Args[2]
+	outputFile := os.Args[3]
+
+	passphrase := os.Getenv("ENCRYPTION_KEY")
+	if passphrase == "" {
+		fmt.Println("Error: ENCRYPTION_KEY environment variable not set")
+		os.Exit(1)
+	}
+
+	inputData, err := os.ReadFile(inputFile)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error reading input file: %v\n", err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("Encrypted: %s\n", encrypted)
-
-	decrypted, err := decrypt(encrypted, key)
-	if err != nil {
-		panic(err)
+	var outputData []byte
+	switch operation {
+	case "encrypt":
+		encrypted, err := encrypt(inputData, passphrase)
+		if err != nil {
+			fmt.Printf("Encryption error: %v\n", err)
+			os.Exit(1)
+		}
+		outputData = []byte(encrypted)
+	case "decrypt":
+		decrypted, err := decrypt(string(inputData), passphrase)
+		if err != nil {
+			fmt.Printf("Decryption error: %v\n", err)
+			os.Exit(1)
+		}
+		outputData = decrypted
+	default:
+		fmt.Println("Error: operation must be 'encrypt' or 'decrypt'")
+		os.Exit(1)
 	}
 
-	fmt.Printf("Decrypted: %s\n", string(decrypted))
+	if err := os.WriteFile(outputFile, outputData, 0644); err != nil {
+		fmt.Printf("Error writing output file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Operation completed successfully. Output written to %s\n", outputFile)
 }
