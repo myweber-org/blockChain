644d893b0dea9fce9d9b1e74d39d821177993ec3
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-03 21:47:09 +0000

    Add user activity logging middleware for tracking API usage

--- pow/user_activity_logger.go
+++ pow/user_activity_logger.go
@@ -1,49 +1,28 @@
-package main
+package middleware
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type ActivityLog struct {
-	Timestamp time.Time `json:"timestamp"`
-	SessionID string    `json:"session_id"`
-	UserID    string    `json:"user_id"`
-	Action    string    `json:"action"`
-	Details   string    `json:"details"`
+type ActivityLogger struct {
+	handler http.Handler
 }
 
-func NewActivityLog(sessionID, userID, action, details string) *ActivityLog {
-	return &ActivityLog{
-		Timestamp: time.Now().UTC(),
-		SessionID: sessionID,
-		UserID:    userID,
-		Action:    action,
-		Details:   details,
-	}
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLog) ToJSON() ([]byte, error) {
-	return json.MarshalIndent(al, "", "  ")
-}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-func LogActivity(logger *log.Logger, sessionID, userID, action, details string) {
-	activity := NewActivityLog(sessionID, userID, action, details)
-	jsonData, err := activity.ToJSON()
-	if err != nil {
-		logger.Printf("Failed to marshal activity log: %v", err)
-		return
-	}
-	logger.Println(string(jsonData))
-}
-
-func main() {
-	logger := log.New(os.Stdout, "ACTIVITY: ", log.LstdFlags)
-	
-	LogActivity(logger, "sess_abc123", "user_789", "LOGIN", "User logged in from IP 192.168.1.100")
-	LogActivity(logger, "sess_abc123", "user_789", "VIEW_PAGE", "Accessed dashboard page")
-	LogActivity(logger, "sess_abc123", "user_789", "LOGOUT", "User logged out after 15 minutes")
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
