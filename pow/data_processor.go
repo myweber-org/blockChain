8fba8558b443edb3bbe708f6d5609b432d852307
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-05 00:32:38 +0000

    Add data validation and sanitization for user input fields

--- pow/data_processor.go
+++ pow/data_processor.go
@@ -86,4 +86,59 @@ func FilterEmptyStrings(slice []string) []string {
 		}
 	}
 	return result
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type UserData struct {
+	Username string
+	Email    string
+	Bio      string
+}
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+func SanitizeInput(input string) string {
+	input = strings.TrimSpace(input)
+	re := regexp.MustCompile(`[<>"'&;]`)
+	return re.ReplaceAllString(input, "")
+}
+
+func ValidateEmail(email string) bool {
+	return emailRegex.MatchString(email)
+}
+
+func ProcessUserData(data UserData) (UserData, error) {
+	data.Username = SanitizeInput(data.Username)
+	data.Email = SanitizeInput(data.Email)
+	data.Bio = SanitizeInput(data.Bio)
+
+	if !ValidateEmail(data.Email) {
+		return data, &InvalidEmailError{Email: data.Email}
+	}
+
+	if len(data.Username) < 3 || len(data.Username) > 20 {
+		return data, &InvalidUsernameError{Username: data.Username}
+	}
+
+	return data, nil
+}
+
+type InvalidEmailError struct {
+	Email string
+}
+
+func (e *InvalidEmailError) Error() string {
+	return "Invalid email format: " + e.Email
+}
+
+type InvalidUsernameError struct {
+	Username string
+}
+
+func (e *InvalidUsernameError) Error() string {
+	return "Username must be between 3 and 20 characters: " + e.Username
 }
