50cc011582c383af55006778b9c04329a930be49
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-06 19:00:53 +0000

    Add currency conversion utility with USD to EUR rate

--- LevelDB/currency_converter.go
+++ LevelDB/currency_converter.go
@@ -70,3 +70,20 @@ func main() {
 
 	fmt.Printf("%.2f %s = %.2f %s\n", amount, fromCurrency, result, toCurrency)
 }
+package main
+
+import (
+	"fmt"
+)
+
+const usdToEurRate = 0.85
+
+func ConvertUSDToEUR(amount float64) float64 {
+	return amount * usdToEurRate
+}
+
+func main() {
+	usdAmount := 100.0
+	eurAmount := ConvertUSDToEUR(usdAmount)
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}
