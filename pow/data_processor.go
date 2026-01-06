e62a41f9793e735940f761293973320ebe66b659
InkspillVerse <inkspill.verse@outlook.com>
2026-01-06 23:17:07 +0000

    Add data validation and sanitization functions for user input processing

--- pow/data_processor.go
+++ pow/data_processor.go
@@ -29,3 +29,33 @@ func (dp *DataProcessor) ProcessUserData(name, email string) (string, string, bo
 	isValid := dp.ValidateEmail(sanitizedEmail)
 	return sanitizedName, sanitizedEmail, isValid
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func TruncateString(s string, maxLength int) string {
+	if len(s) <= maxLength {
+		return s
+	}
+	return s[:maxLength]
+}
+
+func RemoveExtraSpaces(input string) string {
+	space := regexp.MustCompile(`\s+`)
+	return space.ReplaceAllString(strings.TrimSpace(input), " ")
+}
