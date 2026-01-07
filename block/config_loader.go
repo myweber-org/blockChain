f083ebfce269ac19d2c7594d0d54dd565c4b4455
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-07 20:37:25 +0000

    Add configuration loader with YAML parsing support

--- block/config_loader.go
+++ block/config_loader.go
@@ -55,4 +55,36 @@ func validateConfig(cfg *Config) error {
     }
     
     return nil
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config Config
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
 }
