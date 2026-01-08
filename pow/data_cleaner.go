961d269336467bbe440702d964259eb56991bfae
InkspillVerse <inkspill.verse@outlook.com>
2026-01-08 20:07:17 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- pow/data_cleaner.go
+++ pow/data_cleaner.go
@@ -91,4 +91,31 @@ func main() {
 		isValid := cleaner.ValidateEmail(cleaned)
 		fmt.Printf("Original: '%s' -> Cleaned: '%s' (Valid: %v)\n", sample, cleaned, isValid)
 	}
+}package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+    if len(nums) == 0 {
+        return nums
+    }
+
+    seen := make(map[int]bool)
+    result := []int{}
+
+    for _, num := range nums {
+        if !seen[num] {
+            seen[num] = true
+            result = append(result, num)
+        }
+    }
+
+    return result
+}
+
+func main() {
+    data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+    cleaned := RemoveDuplicates(data)
+    fmt.Println("Original:", data)
+    fmt.Println("Cleaned:", cleaned)
 }
