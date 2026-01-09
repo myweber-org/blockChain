80245e17709ef611f46c4fc252ad2498f52349ea
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-09 14:38:49 +0000

    Add user activity logging middleware for API endpoints

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -25,4 +25,45 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ip := r.RemoteAddr
+	method := r.Method
+	path := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from %s %s %s completed in %v", userID, ip, method, path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if auth := r.Header.Get("Authorization"); auth != "" {
+		return auth[:min(8, len(auth))]
+	}
+	return "anonymous"
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
