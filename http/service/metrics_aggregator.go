cd7a11ca9cb5f6a6eb10e8873fb552a44025a309
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-09 00:25:50 +0000

    Add metrics aggregation with sliding window and percentile calculation

---
+++ http/service/metrics_aggregator.go
@@ -0,0 +1,104 @@
+
+package metrics
+
+import (
+	"container/list"
+	"sort"
+	"sync"
+	"time"
+)
+
+type DataPoint struct {
+	Value     float64
+	Timestamp time.Time
+}
+
+type SlidingWindowAggregator struct {
+	windowSize  time.Duration
+	maxPoints   int
+	dataPoints  *list.List
+	mu          sync.RWMutex
+}
+
+func NewSlidingWindowAggregator(windowSize time.Duration, maxPoints int) *SlidingWindowAggregator {
+	return &SlidingWindowAggregator{
+		windowSize: windowSize,
+		maxPoints:  maxPoints,
+		dataPoints: list.New(),
+	}
+}
+
+func (swa *SlidingWindowAggregator) AddValue(value float64) {
+	swa.mu.Lock()
+	defer swa.mu.Unlock()
+
+	now := time.Now()
+	swa.dataPoints.PushBack(DataPoint{
+		Value:     value,
+		Timestamp: now,
+	})
+
+	swa.cleanupOldPoints(now)
+	if swa.dataPoints.Len() > swa.maxPoints {
+		swa.dataPoints.Remove(swa.dataPoints.Front())
+	}
+}
+
+func (swa *SlidingWindowAggregator) cleanupOldPoints(currentTime time.Time) {
+	cutoff := currentTime.Add(-swa.windowSize)
+	for e := swa.dataPoints.Front(); e != nil; {
+		next := e.Next()
+		if dp := e.Value.(DataPoint); dp.Timestamp.Before(cutoff) {
+			swa.dataPoints.Remove(e)
+		}
+		e = next
+	}
+}
+
+func (swa *SlidingWindowAggregator) GetPercentile(p float64) (float64, bool) {
+	swa.mu.RLock()
+	defer swa.mu.RUnlock()
+
+	if swa.dataPoints.Len() == 0 {
+		return 0, false
+	}
+
+	values := make([]float64, 0, swa.dataPoints.Len())
+	for e := swa.dataPoints.Front(); e != nil; e = e.Next() {
+		values = append(values, e.Value.(DataPoint).Value)
+	}
+
+	sort.Float64s(values)
+	index := int(float64(len(values)-1) * p / 100.0)
+	return values[index], true
+}
+
+func (swa *SlidingWindowAggregator) GetStatistics() (min, max, avg float64, count int) {
+	swa.mu.RLock()
+	defer swa.mu.RUnlock()
+
+	if swa.dataPoints.Len() == 0 {
+		return 0, 0, 0, 0
+	}
+
+	var sum float64
+	min = swa.dataPoints.Front().Value.(DataPoint).Value
+	max = min
+	count = 0
+
+	for e := swa.dataPoints.Front(); e != nil; e = e.Next() {
+		value := e.Value.(DataPoint).Value
+		sum += value
+		count++
+
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+
+	avg = sum / float64(count)
+	return min, max, avg, count
+}
