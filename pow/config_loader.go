d88b8c2b4414b25d1838a7c352faf064fdba4fb1
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-09 15:20:22 +0000

    Add configuration file parsing with environment variable support

--- pow/config_loader.go
+++ pow/config_loader.go
@@ -410,4 +410,58 @@ func DefaultConfigPath() string {
     }
 
     return ""
+}package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type AppConfig struct {
+	ServerPort string `json:"server_port"`
+	DBHost     string `json:"db_host"`
+	DBPort     int    `json:"db_port"`
+	DebugMode  bool   `json:"debug_mode"`
+}
+
+func LoadConfig(configPath string) (*AppConfig, error) {
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config AppConfig
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	if port := os.Getenv("APP_PORT"); port != "" {
+		config.ServerPort = port
+	}
+
+	if dbHost := os.Getenv("DB_HOST"); dbHost != "" {
+		config.DBHost = dbHost
+	}
+
+	return &config, nil
+}
+
+func SaveConfig(config *AppConfig, configPath string) error {
+	dir := filepath.Dir(configPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	file, err := os.Create(configPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(config)
 }
