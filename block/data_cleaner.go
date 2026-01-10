9069851dfd60a82a281f9eb04d4a1aed995e18b8
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-10 20:02:03 +0000

    Add data cleaning utility to remove duplicate entries from slices

--- block/data_cleaner.go
+++ block/data_cleaner.go
@@ -1,23 +1,28 @@
-
 package main
 
 import "fmt"
 
-func RemoveDuplicates(nums []int) []int {
-    seen := make(map[int]bool)
-    result := []int{}
-    for _, num := range nums {
-        if !seen[num] {
-            seen[num] = true
-            result = append(result, num)
-        }
-    }
-    return result
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
 }
 
 func main() {
-    input := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
-    cleaned := RemoveDuplicates(input)
-    fmt.Println("Original:", input)
-    fmt.Println("Cleaned:", cleaned)
+	numbers := []int{1, 2, 2, 3, 4, 4, 5}
+	uniqueNumbers := RemoveDuplicates(numbers)
+	fmt.Println("Original:", numbers)
+	fmt.Println("Unique:", uniqueNumbers)
+
+	strings := []string{"apple", "banana", "apple", "orange"}
+	uniqueStrings := RemoveDuplicates(strings)
+	fmt.Println("Original:", strings)
+	fmt.Println("Unique:", uniqueStrings)
 }
