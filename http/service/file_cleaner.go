fddbb5a6a65bfcbecc5bcf723be3ee7fce0ca3c8
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-10 01:13:59 +0000

    Add utility to remove temporary files older than 7 days

--- http/service/file_cleaner.go
+++ http/service/file_cleaner.go
@@ -1,52 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"io/fs"
-	"os"
-	"path/filepath"
-	"time"
-)
-
-const (
-	tempDir      = "/tmp/myapp"
-	maxAgeHours  = 168
+    "os"
+    "path/filepath"
+    "time"
 )
 
 func main() {
-	err := cleanOldFiles(tempDir, maxAgeHours)
-	if err != nil {
-		fmt.Printf("Error cleaning files: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Println("Cleanup completed successfully")
-}
+    tempDir := os.TempDir()
+    cutoff := time.Now().AddDate(0, 0, -7)
 
-func cleanOldFiles(dirPath string, maxAgeHours int) error {
-	cutoffTime := time.Now().Add(-time.Duration(maxAgeHours) * time.Hour)
-
-	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return nil
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		info, err := d.Info()
-		if err != nil {
-			return nil
-		}
-
-		if info.ModTime().Before(cutoffTime) {
-			err := os.Remove(path)
-			if err != nil {
-				fmt.Printf("Failed to remove %s: %v\n", path, err)
-			} else {
-				fmt.Printf("Removed old file: %s\n", path)
-			}
-		}
-		return nil
-	})
+    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return nil
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            os.Remove(path)
+        }
+        return nil
+    })
 }
