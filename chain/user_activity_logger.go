6d5d67d3be06832c1f933c7bfbab438851b5b286
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-10 00:53:27 +0000

    Add user activity logging middleware to track API usage

--- chain/user_activity_logger.go
+++ chain/user_activity_logger.go
@@ -104,4 +104,32 @@ func logActivity(activity ActivityLog, status int) {
 		activity.IP,
 		activity.UserAgent,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf(
+		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+		r.Method,
+		r.URL.Path,
+		duration,
+		time.Now().Format(time.RFC3339),
+	)
 }
