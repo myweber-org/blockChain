51c81726386a2f7c5bd73abfa70aaf3e5dbed3aa
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-11 19:47:04 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- LevelDB/data_cleaner.go
+++ LevelDB/data_cleaner.go
@@ -1,23 +1,24 @@
+
 package main
 
 import "fmt"
 
-func RemoveDuplicates(input []int) []int {
+func removeDuplicates(nums []int) []int {
 	seen := make(map[int]bool)
 	result := []int{}
 
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
 		}
 	}
 	return result
 }
 
 func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5}
-	cleaned := RemoveDuplicates(data)
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := removeDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
