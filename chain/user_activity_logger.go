8dc36de8520a771cdb1dc99540ec6c8bd41fe404
InkspillVerse <inkspill.verse@outlook.com>
2026-01-12 20:11:23 +0000

    Add user activity logging middleware for API endpoints

--- chain/user_activity_logger.go
+++ chain/user_activity_logger.go
@@ -15,12 +15,10 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 }
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
-	
+	start := time.Now()
 	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(startTime)
-	
+	duration := time.Since(start)
+
 	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
