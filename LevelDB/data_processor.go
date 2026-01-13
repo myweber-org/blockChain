101926649b43abe7889ed565382119c7c7869a5a
InkspillVerse <inkspill.verse@outlook.com>
2026-01-13 00:56:37 +0000

    Add utility functions for data validation and transformation

--- LevelDB/data_processor.go
+++ LevelDB/data_processor.go
@@ -137,3 +137,36 @@ func CalculateTotalValue(records []DataRecord) float64 {
 	}
 	return total
 }
+package main
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+func ValidateEmail(email string) error {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, err := regexp.MatchString(pattern, email)
+	if err != nil {
+		return err
+	}
+	if !matched {
+		return errors.New("invalid email format")
+	}
+	return nil
+}
+
+func SanitizeInput(input string) string {
+	input = strings.TrimSpace(input)
+	input = strings.ReplaceAll(input, "<", "&lt;")
+	input = strings.ReplaceAll(input, ">", "&gt;")
+	return input
+}
+
+func TransformToSlug(text string) string {
+	text = strings.ToLower(text)
+	text = regexp.MustCompile(`[^a-z0-9]+`).ReplaceAllString(text, "-")
+	text = strings.Trim(text, "-")
+	return text
+}
