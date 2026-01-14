82cb6f4b9a8ee5fa5d894c16e75a9854c4680a9d
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-14 16:00:28 +0000

    Add user activity logging middleware for API endpoints

--- chain/user_activity_logger.go
+++ chain/user_activity_logger.go
@@ -19,39 +19,10 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
 
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
+		r.RemoteAddr,
 		duration,
-		time.Now().Format(time.RFC3339),
-	)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
-		r.Method,
-		r.URL.Path,
-		duration,
-		time.Now().Format(time.RFC3339),
 	)
 }
