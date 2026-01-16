54f3e85b48106052ecc49b29199a03ed54bd46e7
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-16 23:03:58 +0000

    Add configuration loader with YAML support and environment variable overrides

--- block/config_loader.go
+++ block/config_loader.go
@@ -1,89 +1,75 @@
 package config
 
 import (
-	"encoding/json"
 	"os"
 	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 )
 
-type DatabaseConfig struct {
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Database string `json:"database"`
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+		SSLMode  string `yaml:"ssl_mode" env:"DB_SSL_MODE"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Format string `yaml:"format" env:"LOG_FORMAT"`
+	} `yaml:"logging"`
 }
 
-type ServerConfig struct {
-	Port         int    `json:"port"`
-	ReadTimeout  int    `json:"read_timeout"`
-	WriteTimeout int    `json:"write_timeout"`
-}
-
-type AppConfig struct {
-	Environment string         `json:"environment"`
-	Database    DatabaseConfig `json:"database"`
-	Server      ServerConfig   `json:"server"`
-	LogLevel    string         `json:"log_level"`
-}
-
-func LoadConfig(configPath string) (*AppConfig, error) {
+func LoadConfig(configPath string) (*Config, error) {
 	absPath, err := filepath.Abs(configPath)
 	if err != nil {
 		return nil, err
 	}
 
-	file, err := os.Open(absPath)
+	data, err := os.ReadFile(absPath)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
 
-	var config AppConfig
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&config); err != nil {
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		return nil, err
 	}
 
-	overrideFromEnv(&config)
-
-	return &config, nil
+	overrideFromEnv(&cfg)
+	return &cfg, nil
 }
 
-func overrideFromEnv(config *AppConfig) {
-	if env := os.Getenv("APP_ENV"); env != "" {
-		config.Environment = env
-	}
+func overrideFromEnv(cfg *Config) {
+	setFromEnv(&cfg.Server.Host, "SERVER_HOST")
+	setFromEnvInt(&cfg.Server.Port, "SERVER_PORT")
+	setFromEnv(&cfg.Database.Host, "DB_HOST")
+	setFromEnvInt(&cfg.Database.Port, "DB_PORT")
+	setFromEnv(&cfg.Database.Name, "DB_NAME")
+	setFromEnv(&cfg.Database.User, "DB_USER")
+	setFromEnv(&cfg.Database.Password, "DB_PASSWORD")
+	setFromEnv(&cfg.Database.SSLMode, "DB_SSL_MODE")
+	setFromEnv(&cfg.Logging.Level, "LOG_LEVEL")
+	setFromEnv(&cfg.Logging.Format, "LOG_FORMAT")
+}
 
-	if host := os.Getenv("DB_HOST"); host != "" {
-		config.Database.Host = host
-	}
-	if port := os.Getenv("DB_PORT"); port != "" {
-		if p, err := os.LookupEnv("DB_PORT"); err {
-			config.Database.Port = atoi(p)
-		}
-	}
-	if user := os.Getenv("DB_USER"); user != "" {
-		config.Database.Username = user
-	}
-	if pass := os.Getenv("DB_PASS"); pass != "" {
-		config.Database.Password = pass
-	}
-	if db := os.Getenv("DB_NAME"); db != "" {
-		config.Database.Database = db
-	}
-
-	if level := os.Getenv("LOG_LEVEL"); level != "" {
-		config.LogLevel = level
+func setFromEnv(field *string, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val
 	}
 }
 
-func atoi(s string) int {
-	var result int
-	for _, ch := range s {
-		if ch >= '0' && ch <= '9' {
-			result = result*10 + int(ch-'0')
+func setFromEnvInt(field *int, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		var intVal int
+		if _, err := fmt.Sscanf(val, "%d", &intVal); err == nil {
+			*field = intVal
 		}
 	}
-	return result
 }
