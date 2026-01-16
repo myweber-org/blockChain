ee5d27a4ec1e2dc841561f8072285ea991c4473d
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-16 20:20:59 +0000

    Add user activity logging middleware with rate limiting

--- pow/user_activity_logger.go
+++ pow/user_activity_logger.go
@@ -102,3 +102,47 @@ func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
 }
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type ActivityLogger struct {
+	limiter *rate.Limiter
+	logger  *log.Logger
+}
+
+func NewActivityLogger(rps int, logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{
+		limiter: rate.NewLimiter(rate.Limit(rps), rps),
+		logger:  logger,
+	}
+}
+
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !al.limiter.Allow() {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		duration := time.Since(start)
+
+		al.logger.Printf(
+			"timestamp=%s method=%s path=%s remote_addr=%s duration=%s user_agent=%s",
+			time.Now().Format(time.RFC3339),
+			r.Method,
+			r.URL.Path,
+			r.RemoteAddr,
+			duration.String(),
+			r.UserAgent(),
+		)
+	})
+}
