31b938fa71f53cb879ae5a19717236f0138bd13c
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-17 14:26:02 +0000

    Add session cleanup job to remove expired sessions from the database

--- block/session_cleanup.go
+++ block/session_cleanup.go
@@ -133,4 +133,54 @@ func cleanupExpiredSessions(ctx context.Context, db *database.DB) error {
 
 	log.Printf("Cleaned up %d expired sessions", rowsAffected)
 	return nil
+}package main
+
+import (
+    "context"
+    "database/sql"
+    "log"
+    "time"
+)
+
+func cleanupExpiredSessions(db *sql.DB) error {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    query := `DELETE FROM user_sessions WHERE expires_at < $1`
+    result, err := db.ExecContext(ctx, query, time.Now())
+    if err != nil {
+        return err
+    }
+
+    rowsAffected, err := result.RowsAffected()
+    if err != nil {
+        log.Printf("Failed to get rows affected: %v", err)
+    } else {
+        log.Printf("Cleaned up %d expired sessions", rowsAffected)
+    }
+
+    return nil
+}
+
+func startSessionCleanupJob(db *sql.DB, interval time.Duration) {
+    ticker := time.NewTicker(interval)
+    defer ticker.Stop()
+
+    for range ticker.C {
+        if err := cleanupExpiredSessions(db); err != nil {
+            log.Printf("Session cleanup failed: %v", err)
+        }
+    }
+}
+
+func main() {
+    db, err := sql.Open("postgres", "postgresql://user:pass@localhost/dbname")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    go startSessionCleanupJob(db, 1*time.Hour)
+
+    select {}
 }
