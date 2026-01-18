f7a8069f89f32d943c181d1c7a60a9fe6976965d
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-18 22:51:36 +0000

    Add user activity logging middleware to track API usage

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -7,100 +7,6 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"ACTIVITY: method=%s path=%s status=%d duration=%s remote=%s user_agent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLog struct {
-	UserID    string
-	Endpoint  string
-	Method    string
-	Timestamp time.Time
-	IPAddress string
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		userID := "anonymous"
-		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-			userID = extractUserIDFromToken(authHeader)
-		}
-
-		activity := ActivityLog{
-			UserID:    userID,
-			Endpoint:  r.URL.Path,
-			Method:    r.Method,
-			Timestamp: start,
-			IPAddress: r.RemoteAddr,
-		}
-
-		log.Printf("Activity: %s %s by %s from %s", 
-			activity.Method, 
-			activity.Endpoint, 
-			activity.UserID, 
-			activity.IPAddress)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func extractUserIDFromToken(token string) string {
-	return "user123"
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
 	handler http.Handler
 }
 
@@ -113,11 +19,11 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
 
-	log.Printf("[%s] %s %s - %s - Duration: %v",
-		time.Now().Format(time.RFC3339),
+	log.Printf(
+		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
 		duration,
+		time.Now().Format(time.RFC3339),
 	)
 }
