30c85c57c4042219193fe9adba717cf3059d257c
InkspillVerse <inkspill.verse@outlook.com>
2026-01-20 22:36:27 +0000

    Add configuration loader with environment variable support and validation

--- LevelDB/config_loader.go
+++ LevelDB/config_loader.go
@@ -58,3 +58,107 @@ func SaveConfig(configPath string, cfg *Config) error {
     encoder.SetIndent("", "  ")
     return encoder.Encode(cfg)
 }
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	ServerPort int    `env:"SERVER_PORT" default:"8080"`
+	DBHost     string `env:"DB_HOST" default:"localhost"`
+	DBPort     int    `env:"DB_PORT" default:"5432"`
+	DBName     string `env:"DB_NAME" default:"appdb"`
+	DebugMode  bool   `env:"DEBUG_MODE" default:"false"`
+	LogLevel   string `env:"LOG_LEVEL" default:"info"`
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{}
+	
+	v := reflect.ValueOf(cfg).Elem()
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		structField := t.Field(i)
+		
+		envKey := structField.Tag.Get("env")
+		defaultValue := structField.Tag.Get("default")
+		
+		if envKey == "" {
+			continue
+		}
+		
+		envValue := os.Getenv(envKey)
+		if envValue == "" {
+			envValue = defaultValue
+		}
+		
+		if envValue == "" {
+			return nil, fmt.Errorf("environment variable %s is required", envKey)
+		}
+		
+		if err := setFieldValue(field, envValue); err != nil {
+			return nil, fmt.Errorf("failed to set %s: %w", envKey, err)
+		}
+	}
+	
+	return cfg, nil
+}
+
+func setFieldValue(field reflect.Value, value string) error {
+	switch field.Kind() {
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Int:
+		intVal, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		field.SetInt(int64(intVal))
+	case reflect.Bool:
+		boolVal, err := strconv.ParseBool(strings.ToLower(value))
+		if err != nil {
+			return err
+		}
+		field.SetBool(boolVal)
+	default:
+		return errors.New("unsupported field type")
+	}
+	return nil
+}
+
+func (c *Config) Validate() error {
+	if c.ServerPort < 1 || c.ServerPort > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	
+	if c.DBPort < 1 || c.DBPort > 65535 {
+		return errors.New("database port must be between 1 and 65535")
+	}
+	
+	validLogLevels := map[string]bool{
+		"debug": true,
+		"info":  true,
+		"warn":  true,
+		"error": true,
+	}
+	
+	if !validLogLevels[strings.ToLower(c.LogLevel)] {
+		return errors.New("invalid log level")
+	}
+	
+	return nil
+}
+
+func (c *Config) String() string {
+	data, _ := json.MarshalIndent(c, "", "  ")
+	return string(data)
+}
