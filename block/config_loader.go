b01b2abb071d7fca3b0474e95f767f09b8e45832
InkspillVerse <inkspill.verse@outlook.com>
2026-01-22 01:21:19 +0000

    Add configuration loader with YAML parsing support

--- block/config_loader.go
+++ block/config_loader.go
@@ -54,3 +54,54 @@ func getEnvAsSlice(key string, defaultValue []string, sep string) []string {
     }
     return strings.Split(valueStr, sep)
 }
+package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+    Logging struct {
+        Level  string `yaml:"level"`
+        Output string `yaml:"output"`
+    } `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
+}
+
+func (c *AppConfig) Validate() error {
+    if c.Server.Host == "" {
+        return fmt.Errorf("server host cannot be empty")
+    }
+    if c.Server.Port <= 0 || c.Server.Port > 65535 {
+        return fmt.Errorf("invalid server port: %d", c.Server.Port)
+    }
+    if c.Database.Host == "" {
+        return fmt.Errorf("database host cannot be empty")
+    }
+    return nil
+}
