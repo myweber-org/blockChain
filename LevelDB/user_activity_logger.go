7e91b01fd2bfa433aca28b762715b107bdf17728
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-22 01:28:54 +0000

    Add user activity logging with timestamp and action type

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -46,4 +46,76 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Resource  string    `json:"resource"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+	encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{
+		logFile: file,
+		encoder: json.NewEncoder(file),
+	}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, action, resource string) error {
+	logEntry := ActivityLog{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Action:    action,
+		Resource:  resource,
+	}
+	return al.encoder.Encode(logEntry)
+}
+
+func (al *ActivityLogger) Close() error {
+	return al.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	activities := []struct {
+		userID   string
+		action   string
+		resource string
+	}{
+		{"user_001", "LOGIN", "auth_system"},
+		{"user_002", "CREATE", "document_123"},
+		{"user_001", "UPDATE", "profile_settings"},
+		{"user_003", "DELETE", "comment_456"},
+	}
+
+	for _, act := range activities {
+		if err := logger.LogActivity(act.userID, act.action, act.resource); err != nil {
+			fmt.Printf("Failed to log activity: %v\n", err)
+		} else {
+			fmt.Printf("Logged: %s performed %s on %s\n", act.userID, act.action, act.resource)
+		}
+	}
 }
