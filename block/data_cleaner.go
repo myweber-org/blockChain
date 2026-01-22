afd0534cbdebf06d269720da0e50ada171485346
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-22 01:03:52 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- block/data_cleaner.go
+++ block/data_cleaner.go
@@ -20,4 +20,16 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package datautils
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
 }
