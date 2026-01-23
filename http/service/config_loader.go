117086f55bc6c986bc3e0cb366d49be89647c610
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-23 20:24:39 +0000

    Add configuration loader with YAML parsing and environment variable support

--- http/service/config_loader.go
+++ http/service/config_loader.go
@@ -56,4 +56,78 @@ func overrideFromEnv(c *Config) {
 	if val := os.Getenv("LOG_LEVEL"); val != "" {
 		c.LogLevel = val
 	}
+}package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Driver   string `yaml:"driver" env:"DB_DRIVER"`
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		Username string `yaml:"username" env:"DB_USERNAME"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&cfg)
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) {
+	setFromEnv(&cfg.Server.Host, "SERVER_HOST")
+	setFromEnvInt(&cfg.Server.Port, "SERVER_PORT")
+	setFromEnv(&cfg.Database.Driver, "DB_DRIVER")
+	setFromEnv(&cfg.Database.Host, "DB_HOST")
+	setFromEnvInt(&cfg.Database.Port, "DB_PORT")
+	setFromEnv(&cfg.Database.Name, "DB_NAME")
+	setFromEnv(&cfg.Database.Username, "DB_USERNAME")
+	setFromEnv(&cfg.Database.Password, "DB_PASSWORD")
+	setFromEnv(&cfg.Logging.Level, "LOG_LEVEL")
+	setFromEnv(&cfg.Logging.Output, "LOG_OUTPUT")
+}
+
+func setFromEnv(field *string, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val
+	}
+}
+
+func setFromEnvInt(field *int, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		var intVal int
+		if _, err := fmt.Sscanf(val, "%d", &intVal); err == nil {
+			*field = intVal
+		}
+	}
 }
