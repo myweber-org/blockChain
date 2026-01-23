39973d58a23010b6661cf63cb9c5ed05c17bf6f7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-23 15:01:22 +0000

    Add user activity logging middleware for tracking API requests

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -118,4 +118,50 @@ func main() {
 			fmt.Printf("Logged: %s performed %s on %s\n", act.userID, act.action, act.resource)
 		}
 	}
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf(
+		"%s %s %d %s %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+		r.RemoteAddr,
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
