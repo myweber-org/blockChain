853dda7eb927b103461a1dc684e1c33d61a2bef6
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-24 01:36:54 +0000

    Add utility to remove temporary files older than 7 days

--- chain/file_cleaner.go
+++ chain/file_cleaner.go
@@ -22,4 +22,34 @@ func main() {
         }
         return nil
     })
+}package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().Add(-7 * 24 * time.Hour)
+
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing old file: %s\n", path)
+			os.Remove(path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error cleaning temp directory: %v\n", err)
+	}
 }
