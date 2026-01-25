e887ed772b6c67eedef94eef04cedc5d0be2bceb
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-25 00:54:53 +0000

    Implement log rotation with size-based file splitting and compression

--- chain/log_rotator.go
+++ chain/log_rotator.go
@@ -1,142 +1,139 @@
+
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
+    "compress/gzip"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sync"
+    "time"
 )
 
-const (
-	maxFileSize = 10 * 1024 * 1024
-	maxBackups  = 5
-	logDir      = "./logs"
-)
-
-type RotatingLogger struct {
-	currentFile *os.File
-	currentSize int64
-	baseName    string
+type RotatingLog struct {
+    mu          sync.Mutex
+    filePath    string
+    maxSize     int64
+    currentSize int64
+    file        *os.File
+    sequence    int
 }
 
-func NewRotatingLogger(name string) (*RotatingLogger, error) {
-	if err := os.MkdirAll(logDir, 0755); err != nil {
-		return nil, err
-	}
+func NewRotatingLog(path string, maxSizeMB int) (*RotatingLog, error) {
+    maxSize := int64(maxSizeMB) * 1024 * 1024
+    file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
 
-	baseName := filepath.Join(logDir, name)
-	logger := &RotatingLogger{baseName: baseName}
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return nil, err
+    }
 
-	if err := logger.openCurrentFile(); err != nil {
-		return nil, err
-	}
-
-	return logger, nil
+    return &RotatingLog{
+        filePath:    path,
+        maxSize:     maxSize,
+        currentSize: info.Size(),
+        file:        file,
+        sequence:    0,
+    }, nil
 }
 
-func (rl *RotatingLogger) openCurrentFile() error {
-	filename := rl.baseName + ".log"
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		return err
-	}
+func (rl *RotatingLog) Write(p []byte) (int, error) {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
 
-	info, err := file.Stat()
-	if err != nil {
-		file.Close()
-		return err
-	}
+    if rl.currentSize+int64(len(p)) > rl.maxSize {
+        if err := rl.rotate(); err != nil {
+            return 0, err
+        }
+    }
 
-	rl.currentFile = file
-	rl.currentSize = info.Size()
-	return nil
+    n, err := rl.file.Write(p)
+    if err == nil {
+        rl.currentSize += int64(n)
+    }
+    return n, err
 }
 
-func (rl *RotatingLogger) Write(p []byte) (int, error) {
-	if rl.currentSize+int64(len(p)) > maxFileSize {
-		if err := rl.rotate(); err != nil {
-			return 0, err
-		}
-	}
+func (rl *RotatingLog) rotate() error {
+    if rl.file != nil {
+        rl.file.Close()
+    }
 
-	n, err := rl.currentFile.Write(p)
-	if err == nil {
-		rl.currentSize += int64(n)
-	}
-	return n, err
+    timestamp := time.Now().Format("20060102_150405")
+    archiveName := fmt.Sprintf("%s.%s.%d.gz", rl.filePath, timestamp, rl.sequence)
+    rl.sequence++
+
+    if err := rl.compressFile(rl.filePath, archiveName); err != nil {
+        return err
+    }
+
+    if err := os.Truncate(rl.filePath, 0); err != nil {
+        return err
+    }
+
+    file, err := os.OpenFile(rl.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+
+    rl.file = file
+    rl.currentSize = 0
+    return nil
 }
 
-func (rl *RotatingLogger) rotate() error {
-	if rl.currentFile != nil {
-		rl.currentFile.Close()
-	}
+func (rl *RotatingLog) compressFile(source, target string) error {
+    srcFile, err := os.Open(source)
+    if err != nil {
+        return err
+    }
+    defer srcFile.Close()
 
-	timestamp := time.Now().Format("20060102_150405")
-	backupName := fmt.Sprintf("%s_%s.log", rl.baseName, timestamp)
-	currentName := rl.baseName + ".log"
+    destFile, err := os.Create(target)
+    if err != nil {
+        return err
+    }
+    defer destFile.Close()
 
-	if err := os.Rename(currentName, backupName); err != nil {
-		return err
-	}
+    gzWriter := gzip.NewWriter(destFile)
+    defer gzWriter.Close()
 
-	if err := rl.openCurrentFile(); err != nil {
-		return err
-	}
-
-	go rl.cleanupOldFiles()
-	return nil
+    _, err = io.Copy(gzWriter, srcFile)
+    return err
 }
 
-func (rl *RotatingLogger) cleanupOldFiles() error {
-	pattern := rl.baseName + "_*.log"
-	matches, err := filepath.Glob(pattern)
-	if err != nil {
-		return err
-	}
+func (rl *RotatingLog) Close() error {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
 
-	if len(matches) <= maxBackups {
-		return nil
-	}
-
-	oldestFiles := matches[:len(matches)-maxBackups]
-	for _, file := range oldestFiles {
-		if err := os.Remove(file); err != nil {
-			fmt.Printf("Failed to remove old log file %s: %v\n", file, err)
-		}
-	}
-	return nil
-}
-
-func (rl *RotatingLogger) Close() error {
-	if rl.currentFile != nil {
-		return rl.currentFile.Close()
-	}
-	return nil
+    if rl.file != nil {
+        return rl.file.Close()
+    }
+    return nil
 }
 
 func main() {
-	logger, err := NewRotatingLogger("app")
-	if err != nil {
-		fmt.Printf("Failed to create logger: %v\n", err)
-		return
-	}
-	defer logger.Close()
+    logFile := "application.log"
+    rotator, err := NewRotatingLog(logFile, 10)
+    if err != nil {
+        fmt.Printf("Failed to create log rotator: %v\n", err)
+        return
+    }
+    defer rotator.Close()
 
-	for i := 0; i < 1000; i++ {
-		message := fmt.Sprintf("[%s] Log entry %d: %s\n",
-			time.Now().Format(time.RFC3339),
-			i,
-			strings.Repeat("X", 1024))
+    for i := 0; i < 1000; i++ {
+        message := fmt.Sprintf("Log entry %d: %s\n", i, time.Now().Format(time.RFC3339))
+        if _, err := rotator.Write([]byte(message)); err != nil {
+            fmt.Printf("Write error: %v\n", err)
+        }
+        time.Sleep(10 * time.Millisecond)
+    }
 
-		if _, err := logger.Write([]byte(message)); err != nil {
-			fmt.Printf("Write error: %v\n", err)
-			break
-		}
-
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	fmt.Println("Log rotation test completed")
+    fmt.Println("Log rotation test completed")
+    files, _ := filepath.Glob("*.gz")
+    fmt.Printf("Created %d compressed archives\n", len(files))
 }
