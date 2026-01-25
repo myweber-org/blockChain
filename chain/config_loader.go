e43777b1a35401903f2b604ba21c8634ba2067cb
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-25 16:55:43 +0000

    Add configuration loader with YAML parsing support

--- chain/config_loader.go
+++ chain/config_loader.go
@@ -1,66 +1,41 @@
 package config
 
 import (
-    "fmt"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+    "io"
+    "os"
+
+    "gopkg.in/yaml.v3"
 )
 
-type DatabaseConfig struct {
-    Host     string `yaml:"host"`
-    Port     int    `yaml:"port"`
-    Username string `yaml:"username"`
-    Password string `yaml:"password"`
-    Name     string `yaml:"name"`
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Driver   string `yaml:"driver"`
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
 }
 
-type ServerConfig struct {
-    Port int    `yaml:"port"`
-    Mode string `yaml:"mode"`
-}
-
-type AppConfig struct {
-    Database DatabaseConfig `yaml:"database"`
-    Server   ServerConfig   `yaml:"server"`
-    Features []string       `yaml:"features"`
-}
-
-func LoadConfig(filePath string) (*AppConfig, error) {
-    data, err := ioutil.ReadFile(filePath)
+func LoadConfig(path string) (*Config, error) {
+    file, err := os.Open(path)
     if err != nil {
-        return nil, fmt.Errorf("failed to read config file: %w", err)
+        return nil, err
+    }
+    defer file.Close()
+
+    data, err := io.ReadAll(file)
+    if err != nil {
+        return nil, err
     }
 
-    var config AppConfig
-    if err := yaml.Unmarshal(data, &config); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    var cfg Config
+    if err := yaml.Unmarshal(data, &cfg); err != nil {
+        return nil, err
     }
 
-    if err := validateConfig(&config); err != nil {
-        return nil, fmt.Errorf("config validation failed: %w", err)
-    }
-
-    return &config, nil
-}
-
-func validateConfig(config *AppConfig) error {
-    if config.Database.Host == "" {
-        return fmt.Errorf("database host cannot be empty")
-    }
-    if config.Database.Port <= 0 || config.Database.Port > 65535 {
-        return fmt.Errorf("invalid database port: %d", config.Database.Port)
-    }
-    if config.Server.Port <= 0 || config.Server.Port > 65535 {
-        return fmt.Errorf("invalid server port: %d", config.Server.Port)
-    }
-    return nil
-}
-
-func (c *AppConfig) GetDatabaseDSN() string {
-    return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True",
-        c.Database.Username,
-        c.Database.Password,
-        c.Database.Host,
-        c.Database.Port,
-        c.Database.Name)
+    return &cfg, nil
 }
