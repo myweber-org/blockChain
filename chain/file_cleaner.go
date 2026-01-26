666689ef0ae1f112a63934345ac89fa347e4c2d9
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-26 18:53:55 +0000

    Add utility to clean up old log files in a directory

--- chain/file_cleaner.go
+++ chain/file_cleaner.go
@@ -1,55 +1,60 @@
 package main
 
 import (
-    "os"
-    "path/filepath"
-    "time"
-)
-
-func main() {
-    tempDir := os.TempDir()
-    cutoff := time.Now().AddDate(0, 0, -7)
-
-    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return nil
-        }
-        if info.IsDir() {
-            return nil
-        }
-        if info.ModTime().Before(cutoff) {
-            os.Remove(path)
-        }
-        return nil
-    })
-}package main
-
-import (
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"time"
 )
 
-func main() {
-	tempDir := os.TempDir()
-	cutoffTime := time.Now().Add(-7 * 24 * time.Hour)
+const (
+	retentionDays = 30
+	logExtension  = ".log"
+)
 
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_cleaner <directory>")
+		os.Exit(1)
+	}
+
+	dir := os.Args[1]
+	err := cleanOldLogs(dir)
+	if err != nil {
+		fmt.Printf("Error cleaning logs: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Log cleanup completed successfully")
+}
+
+func cleanOldLogs(dir string) error {
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
+
+	return filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
 			return nil
 		}
-		if info.IsDir() {
+
+		if filepath.Ext(path) != logExtension {
 			return nil
 		}
+
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+
 		if info.ModTime().Before(cutoffTime) {
-			fmt.Printf("Removing old file: %s\n", path)
-			os.Remove(path)
+			fmt.Printf("Removing old log file: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
 		}
+
 		return nil
 	})
-
-	if err != nil {
-		fmt.Printf("Error cleaning temp directory: %v\n", err)
-	}
 }
