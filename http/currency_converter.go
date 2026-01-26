98853fc47a6e23b8204603175439637fdfc16d66
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-26 02:32:12 +0000

    Add currency conversion utility with USD to EUR rate

--- http/currency_converter.go
+++ http/currency_converter.go
@@ -302,4 +302,33 @@ func main() {
 	
 	fmt.Printf("%.2f USD = %.2f EUR\n", amount, converted)
 	fmt.Printf("Supported pairs: %v\n", converter.GetSupportedPairs())
+}package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const usdToEurRate = 0.92
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run currency_converter.go <amount_in_usd>")
+		os.Exit(1)
+	}
+
+	usdAmount, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fmt.Printf("Invalid amount: %v\n", err)
+		os.Exit(1)
+	}
+
+	if usdAmount < 0 {
+		fmt.Println("Amount cannot be negative")
+		os.Exit(1)
+	}
+
+	eurAmount := usdAmount * usdToEurRate
+	fmt.Printf("%.2f USD = %.2f EUR (Rate: %.2f)\n", usdAmount, eurAmount, usdToEurRate)
 }
