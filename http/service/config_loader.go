869b0830c2c669bc94943389b7422e5696ce6f2f
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-26 00:47:02 +0000

    Add configuration file loader with YAML parsing support

--- http/service/config_loader.go
+++ http/service/config_loader.go
@@ -180,4 +180,49 @@ func (c *Config) Validate() error {
 		return os.ErrInvalid
 	}
 	return nil
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Name     string `yaml:"name"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config Config
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
+}
+
+func (c *Config) Validate() error {
+    if c.Server.Host == "" {
+        return fmt.Errorf("server host cannot be empty")
+    }
+    if c.Server.Port <= 0 {
+        return fmt.Errorf("server port must be positive")
+    }
+    if c.Database.Name == "" {
+        return fmt.Errorf("database name cannot be empty")
+    }
+    return nil
 }
