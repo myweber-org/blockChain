92dc9238587bbb71c8a513bc1d6fc24ed9d9d01b
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-26 01:41:56 +0000

    Add user activity logging middleware for API endpoints

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -6,59 +6,40 @@ import (
 	"time"
 )
 
-type ActivityLog struct {
-	UserID    string
-	Endpoint  string
-	Method    string
-	Timestamp time.Time
-	IPAddress string
+type ActivityLogger struct {
+	handler http.Handler
 }
 
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		userID := extractUserID(r)
-		ip := getClientIP(r)
-
-		logEntry := ActivityLog{
-			UserID:    userID,
-			Endpoint:  r.URL.Path,
-			Method:    r.Method,
-			Timestamp: start,
-			IPAddress: ip,
-		}
-
-		logActivity(logEntry)
-
-		next.ServeHTTP(w, r)
-	})
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func extractUserID(r *http.Request) string {
-	if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-		return parseTokenForUserID(authHeader)
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
 	}
-	return "anonymous"
+
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(start)
+	log.Printf(
+		"%s %s %d %s %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+		r.RemoteAddr,
+	)
 }
 
-func parseTokenForUserID(token string) string {
-	return "user123"
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func getClientIP(r *http.Request) string {
-	forwarded := r.Header.Get("X-Forwarded-For")
-	if forwarded != "" {
-		return forwarded
-	}
-	return r.RemoteAddr
-}
-
-func logActivity(entry ActivityLog) {
-	log.Printf("ACTIVITY: User %s %s %s from %s at %v",
-		entry.UserID,
-		entry.Method,
-		entry.Endpoint,
-		entry.IPAddress,
-		entry.Timestamp.Format(time.RFC3339))
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
