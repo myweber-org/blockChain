c9ee801de00df33c33efdf4a8c1219e2c8571955
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-27 21:48:31 +0000

    Add user activity logging with timestamps and action types

--- chain/user_activity_logger.go
+++ chain/user_activity_logger.go
@@ -128,4 +128,89 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityType string
+
+const (
+    Login    ActivityType = "LOGIN"
+    Logout   ActivityType = "LOGOUT"
+    Purchase ActivityType = "PURCHASE"
+    View     ActivityType = "VIEW"
+)
+
+type UserActivity struct {
+    UserID    string       `json:"user_id"`
+    Action    ActivityType `json:"action"`
+    Timestamp time.Time    `json:"timestamp"`
+    Details   string       `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID string, action ActivityType, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.Marshal(activity)
+    if err != nil {
+        return err
+    }
+
+    _, err = l.logFile.Write(append(data, '\n'))
+    return err
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activities.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    activities := []struct {
+        userID  string
+        action  ActivityType
+        details string
+    }{
+        {"user123", Login, "Successful authentication"},
+        {"user123", View, "Product page: laptop-x1"},
+        {"user123", Purchase, "Order #78910: laptop-x1"},
+        {"user456", Login, "New device detected"},
+        {"user123", Logout, "Session timeout"},
+    }
+
+    for _, a := range activities {
+        if err := logger.LogActivity(a.userID, a.action, a.details); err != nil {
+            log.Printf("Failed to log activity: %v", err)
+        }
+    }
+
+    fmt.Println("Activity logging completed")
 }
