90fb3f77ce3c4d51f1c38da7cd1ae6752130d699
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-27 23:34:56 +0000

    Add user activity logging middleware for API endpoints

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -26,4 +26,38 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ipAddress := r.RemoteAddr
+	method := r.Method
+	path := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from %s performed %s %s in %v", userID, ipAddress, method, path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if userID := r.Header.Get("X-User-ID"); userID != "" {
+		return userID
+	}
+	return "anonymous"
 }
