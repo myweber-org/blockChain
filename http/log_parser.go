8336e4d238762eec0851e6cf718bb71fb124b145
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-28 19:29:42 +0000

    Add log parsing utility to extract error messages from application logs

--- http/log_parser.go
+++ http/log_parser.go
@@ -164,4 +164,79 @@ func main() {
 		fmt.Printf("  Time: %s\n", entry.Timestamp)
 		fmt.Printf("  Message: %s\n\n", entry.Message)
 	}
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type LogEntry struct {
+	Timestamp string
+	Level     string
+	Message   string
+}
+
+func parseLogLine(line string) (LogEntry, error) {
+	pattern := `^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) \[(\w+)\] (.+)$`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindStringSubmatch(line)
+
+	if len(matches) != 4 {
+		return LogEntry{}, fmt.Errorf("invalid log format")
+	}
+
+	return LogEntry{
+		Timestamp: matches[1],
+		Level:     matches[2],
+		Message:   matches[3],
+	}, nil
+}
+
+func extractErrors(logFile string) ([]LogEntry, error) {
+	file, err := os.Open(logFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var errors []LogEntry
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		entry, err := parseLogLine(scanner.Text())
+		if err != nil {
+			continue
+		}
+		if strings.ToUpper(entry.Level) == "ERROR" {
+			errors = append(errors, entry)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return errors, nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: log_parser <logfile>")
+		os.Exit(1)
+	}
+
+	errors, err := extractErrors(os.Args[1])
+	if err != nil {
+		fmt.Printf("Error parsing log file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Found %d error(s):\n", len(errors))
+	for i, entry := range errors {
+		fmt.Printf("%d. [%s] %s: %s\n", i+1, entry.Timestamp, entry.Level, entry.Message)
+	}
 }
