5b04a347f7d59d83833814f62982826285c95fc9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-28 01:00:02 +0000

    Add session cleanup cron job to remove expired sessions daily

--- http/session_cleanup.go
+++ http/session_cleanup.go
@@ -71,3 +71,63 @@ func main() {
 	
 	log.Println("Session cleanup service would start here")
 }
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type Session struct {
+	ID        string
+	UserID    string
+	ExpiresAt time.Time
+}
+
+type SessionStore interface {
+	GetAllSessions() ([]Session, error)
+	DeleteSession(id string) error
+}
+
+func cleanupExpiredSessions(store SessionStore) error {
+	sessions, err := store.GetAllSessions()
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	deletedCount := 0
+
+	for _, session := range sessions {
+		if session.ExpiresAt.Before(now) {
+			err := store.DeleteSession(session.ID)
+			if err != nil {
+				log.Printf("Failed to delete session %s: %v", session.ID, err)
+				continue
+			}
+			deletedCount++
+		}
+	}
+
+	log.Printf("Cleaned up %d expired sessions", deletedCount)
+	return nil
+}
+
+func scheduleCleanup(store SessionStore, interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		err := cleanupExpiredSessions(store)
+		if err != nil {
+			log.Printf("Session cleanup failed: %v", err)
+		}
+	}
+}
+
+func main() {
+	// Implementation would initialize SessionStore
+	// and start the cleanup scheduler
+	// store := NewSessionStore()
+	// scheduleCleanup(store, 24*time.Hour)
+}
