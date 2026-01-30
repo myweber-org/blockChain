5e207ca013843e4c09f6c0dc562e50f297475e3d
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-30 02:10:46 +0000

    Add user activity logging with timestamp and action type

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -52,4 +52,52 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Resource  string    `json:"resource"`
+}
+
+func NewActivityLog(userID, action, resource string) *ActivityLog {
+	return &ActivityLog{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Action:    action,
+		Resource:  resource,
+	}
+}
+
+func (al *ActivityLog) Save() error {
+	file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data, err := json.Marshal(al)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(append(data, '\n'))
+	return err
+}
+
+func main() {
+	logger := NewActivityLog("user123", "login", "/dashboard")
+	if err := logger.Save(); err != nil {
+		log.Fatal("Failed to save activity log:", err)
+	}
+	fmt.Println("Activity logged successfully")
 }
