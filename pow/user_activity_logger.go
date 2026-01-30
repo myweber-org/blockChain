8ebffd92b35254584dbf7aa76b1af5012f7fe70c
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-30 19:24:12 +0000

    Add user activity logging middleware with request details and timestamp

--- pow/user_activity_logger.go
+++ pow/user_activity_logger.go
@@ -6,85 +6,31 @@ import (
 	"time"
 )
 
-type ActivityLog struct {
-	UserID    string
-	IPAddress string
-	Method    string
-	Path      string
-	Timestamp time.Time
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
 
 func ActivityLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
 
-		userID := "anonymous"
-		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-			userID = extractUserID(authHeader)
-		}
+		next.ServeHTTP(rw, r)
 
-		activity := ActivityLog{
-			UserID:    userID,
-			IPAddress: r.RemoteAddr,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			Timestamp: start,
-		}
-
-		log.Printf("Activity: %s %s by %s from %s", activity.Method, activity.Path, activity.UserID, activity.IPAddress)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func extractUserID(token string) string {
-	return "user_" + token[:8]
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
 		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"%s %s %d %s %s",
+		log.Printf(
+			"[%s] %s %s %d %v",
+			time.Now().Format(time.RFC3339),
 			r.Method,
 			r.URL.Path,
-			recorder.statusCode,
+			rw.statusCode,
 			duration,
-			r.RemoteAddr,
 		)
 	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
 }
