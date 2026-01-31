878bd8a759d399a09f7199b3401fb1994d6313d6
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-31 14:51:58 +0000

    Add user activity logging middleware for tracking API usage

--- pow/user_activity_logger.go
+++ pow/user_activity_logger.go
@@ -33,4 +33,50 @@ func ActivityLogger(next http.Handler) http.Handler {
 			duration,
 		)
 	})
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		userAgent := r.UserAgent()
+		clientIP := r.RemoteAddr
+
+		lrw := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		next.ServeHTTP(lrw, r)
+
+		duration := time.Since(start)
+		al.Logger.Printf(
+			"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s | IP: %s",
+			r.Method,
+			r.URL.Path,
+			lrw.statusCode,
+			duration,
+			userAgent,
+			clientIP,
+		)
+	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
