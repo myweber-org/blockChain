ad6a91702bd20acfe2d9c306b416d3a1cf78440e
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-01 19:06:23 +0000

    Add user activity logging middleware for tracking API requests

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -88,4 +88,71 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	Method    string
+	Path      string
+	UserAgent string
+	IP        string
+	Duration  time.Duration
+}
+
+type ActivityLogger struct {
+	logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{logger: logger}
+}
+
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, r)
+
+		duration := time.Since(start)
+
+		activity := ActivityLog{
+			Timestamp: start,
+			Method:    r.Method,
+			Path:      r.URL.Path,
+			UserAgent: r.UserAgent(),
+			IP:        r.RemoteAddr,
+			Duration:  duration,
+		}
+
+		al.logger.Printf(
+			"Activity: %s %s %s %s %d %v",
+			activity.Timestamp.Format(time.RFC3339),
+			activity.Method,
+			activity.Path,
+			activity.IP,
+			recorder.statusCode,
+			activity.Duration,
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
