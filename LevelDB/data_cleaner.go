561866928114840000821cb3c6f49490f04322de
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-01 15:42:03 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- LevelDB/data_cleaner.go
+++ LevelDB/data_cleaner.go
@@ -21,4 +21,58 @@ func main() {
 	cleaned := RemoveDuplicates(input)
 	fmt.Printf("Original: %v\n", input)
 	fmt.Printf("Cleaned: %v\n", cleaned)
+}package datautil
+
+import "sort"
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	seen := make(map[T]bool)
+	result := make([]T, 0, len(slice))
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+func RemoveDuplicatesSorted[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	sort.Slice(slice, func(i, j int) bool {
+		// Convert to string for comparison to satisfy comparable constraint
+		return toString(slice[i]) < toString(slice[j])
+	})
+
+	result := slice[:1]
+	for i := 1; i < len(slice); i++ {
+		if slice[i] != slice[i-1] {
+			result = append(result, slice[i])
+		}
+	}
+
+	return result
+}
+
+func toString(v interface{}) string {
+	// Helper function for sorting comparable types
+	switch val := v.(type) {
+	case string:
+		return val
+	case int:
+		return strconv.Itoa(val)
+	case float64:
+		return strconv.FormatFloat(val, 'f', -1, 64)
+	default:
+		return fmt.Sprintf("%v", val)
+	}
 }
