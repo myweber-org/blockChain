8da11b9f8502fa8a7f7aa82d4885535f1f9757c0
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-02 16:46:04 +0000

    Add utility to remove temporary files older than 7 days

--- block/file_cleaner.go
+++ block/file_cleaner.go
@@ -58,4 +58,45 @@ func main() {
 	}
 
 	fmt.Printf("Duplicate removal complete. Output written to %s\n", outputFile)
+}package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const tempDir = "/tmp/myapp"
+const retentionDays = 7
+
+func main() {
+	err := cleanOldFiles(tempDir, retentionDays)
+	if err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string, days int) error {
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return err
+	}
+
+	cutoffTime := time.Now().AddDate(0, 0, -days)
+	for _, file := range files {
+		if file.ModTime().Before(cutoffTime) {
+			fullPath := filepath.Join(dirPath, file.Name())
+			err := os.Remove(fullPath)
+			if err != nil {
+				fmt.Printf("Failed to remove %s: %v\n", fullPath, err)
+			} else {
+				fmt.Printf("Removed: %s\n", fullPath)
+			}
+		}
+	}
+	return nil
 }
