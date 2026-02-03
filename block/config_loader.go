82d2e59076d6976a9e4e9c60cbef609bd23ac1a7
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-03 22:34:29 +0000

    Add configuration loader with YAML parsing support

--- block/config_loader.go
+++ block/config_loader.go
@@ -228,4 +228,66 @@ func GetEnvConfig() *ServerConfig {
             Name:     os.Getenv("DB_NAME"),
         },
     }
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Name     string `yaml:"name"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(configPath string) (*AppConfig, error) {
+    if configPath == "" {
+        configPath = "config.yaml"
+    }
+
+    file, err := os.Open(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open config file: %w", err)
+    }
+    defer file.Close()
+
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML config: %w", err)
+    }
+
+    return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) error {
+    if config.Server.Host == "" {
+        return fmt.Errorf("server host cannot be empty")
+    }
+    if config.Server.Port <= 0 || config.Server.Port > 65535 {
+        return fmt.Errorf("invalid server port: %d", config.Server.Port)
+    }
+    if config.Database.Host == "" {
+        return fmt.Errorf("database host cannot be empty")
+    }
+    if config.Database.Name == "" {
+        return fmt.Errorf("database name cannot be empty")
+    }
+    return nil
 }
