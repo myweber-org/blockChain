07566cee4aeeb2dfb1f942368f2add4c6a356228
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-03 19:55:04 +0000

    Implement log rotation with size-based file splitting and compression

--- http/log_rotator.go
+++ http/log_rotator.go
@@ -593,4 +593,153 @@ func main() {
     }
 
     fmt.Println("Log rotation test completed")
+}package main
+
+import (
+    "compress/gzip"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "strconv"
+    "strings"
+    "sync"
+    "time"
+)
+
+type RotatingLogger struct {
+    mu          sync.Mutex
+    basePath    string
+    maxSize     int64
+    currentSize int64
+    currentFile *os.File
+    sequence    int
+}
+
+func NewRotatingLogger(basePath string, maxSizeMB int) (*RotatingLogger, error) {
+    maxSize := int64(maxSizeMB) * 1024 * 1024
+    logger := &RotatingLogger{
+        basePath: basePath,
+        maxSize:  maxSize,
+        sequence: 0,
+    }
+
+    if err := logger.openCurrentFile(); err != nil {
+        return nil, err
+    }
+    return logger, nil
+}
+
+func (l *RotatingLogger) openCurrentFile() error {
+    if l.currentFile != nil {
+        l.currentFile.Close()
+    }
+
+    filename := l.basePath
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+
+    stat, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return err
+    }
+
+    l.currentFile = file
+    l.currentSize = stat.Size()
+    l.sequence = l.findLatestSequence()
+    return nil
+}
+
+func (l *RotatingLogger) findLatestSequence() int {
+    pattern := l.basePath + ".*.gz"
+    matches, _ := filepath.Glob(pattern)
+    maxSeq := 0
+    for _, match := range matches {
+        parts := strings.Split(match, ".")
+        if len(parts) < 3 {
+            continue
+        }
+        seq, err := strconv.Atoi(parts[len(parts)-2])
+        if err == nil && seq > maxSeq {
+            maxSeq = seq
+        }
+    }
+    return maxSeq
+}
+
+func (l *RotatingLogger) Write(p []byte) (int, error) {
+    l.mu.Lock()
+    defer l.mu.Unlock()
+
+    if l.currentSize+int64(len(p)) > l.maxSize {
+        if err := l.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := l.currentFile.Write(p)
+    if err == nil {
+        l.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (l *RotatingLogger) rotate() error {
+    if l.currentFile != nil {
+        l.currentFile.Close()
+    }
+
+    l.sequence++
+    archiveName := fmt.Sprintf("%s.%d.%s.gz", l.basePath, l.sequence, time.Now().Format("20060102"))
+
+    source, err := os.Open(l.basePath)
+    if err != nil {
+        return err
+    }
+    defer source.Close()
+
+    dest, err := os.Create(archiveName)
+    if err != nil {
+        return err
+    }
+    defer dest.Close()
+
+    gz := gzip.NewWriter(dest)
+    defer gz.Close()
+
+    if _, err := io.Copy(gz, source); err != nil {
+        return err
+    }
+
+    if err := os.Remove(l.basePath); err != nil {
+        return err
+    }
+
+    return l.openCurrentFile()
+}
+
+func (l *RotatingLogger) Close() error {
+    l.mu.Lock()
+    defer l.mu.Unlock()
+    if l.currentFile != nil {
+        return l.currentFile.Close()
+    }
+    return nil
+}
+
+func main() {
+    logger, err := NewRotatingLogger("app.log", 10)
+    if err != nil {
+        panic(err)
+    }
+    defer logger.Close()
+
+    for i := 0; i < 1000; i++ {
+        logLine := fmt.Sprintf("[%s] Log entry number %d\n", time.Now().Format(time.RFC3339), i)
+        logger.Write([]byte(logLine))
+        time.Sleep(10 * time.Millisecond)
+    }
 }
