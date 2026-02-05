efbcbd30475ede78fcc38408e4876b76a1c74449
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-05 15:10:42 +0000

    Add user activity logging middleware for API endpoints

--- pow/user_activity_logger.go
+++ pow/user_activity_logger.go
@@ -25,4 +25,35 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userAgent := r.Header.Get("User-Agent")
+	ipAddress := r.RemoteAddr
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("Activity: %s %s | IP: %s | Agent: %s | Duration: %v",
+		r.Method,
+		r.URL.Path,
+		ipAddress,
+		userAgent,
+		duration,
+	)
 }
