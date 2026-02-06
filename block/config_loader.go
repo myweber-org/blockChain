20b52a281e3adf0f12bc6f029430592080cffdf1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-06 20:05:07 +0000

    Add configuration loader with environment variable support and validation

--- block/config_loader.go
+++ block/config_loader.go
@@ -148,3 +148,102 @@ func overrideStruct(s interface{}, prefix string) {
 		}
 	}
 }
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	ServerPort int    `env:"SERVER_PORT" default:"8080"`
+	DBHost     string `env:"DB_HOST" default:"localhost"`
+	DBPort     int    `env:"DB_PORT" default:"5432"`
+	DebugMode  bool   `env:"DEBUG_MODE" default:"false"`
+	LogLevel   string `env:"LOG_LEVEL" default:"info"`
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+	
+	v := reflect.ValueOf(cfg).Elem()
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		structField := t.Field(i)
+		
+		envTag := structField.Tag.Get("env")
+		defaultTag := structField.Tag.Get("default")
+		
+		var value string
+		if envVal, exists := os.LookupEnv(envTag); exists {
+			value = envVal
+		} else if defaultTag != "" {
+			value = defaultTag
+		} else {
+			return nil, fmt.Errorf("missing required environment variable: %s", envTag)
+		}
+		
+		if err := setFieldValue(field, structField.Type, value); err != nil {
+			return nil, fmt.Errorf("failed to set field %s: %w", structField.Name, err)
+		}
+	}
+	
+	return cfg, nil
+}
+
+func setFieldValue(field reflect.Value, fieldType reflect.Type, value string) error {
+	switch fieldType.Kind() {
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Int:
+		intVal, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		field.SetInt(int64(intVal))
+	case reflect.Bool:
+		boolVal, err := strconv.ParseBool(strings.ToLower(value))
+		if err != nil {
+			return err
+		}
+		field.SetBool(boolVal)
+	default:
+		return errors.New("unsupported field type")
+	}
+	return nil
+}
+
+func (c *Config) Validate() error {
+	if c.ServerPort < 1 || c.ServerPort > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	
+	if c.DBPort < 1 || c.DBPort > 65535 {
+		return errors.New("database port must be between 1 and 65535")
+	}
+	
+	validLogLevels := map[string]bool{
+		"debug": true,
+		"info":  true,
+		"warn":  true,
+		"error": true,
+	}
+	
+	if !validLogLevels[strings.ToLower(c.LogLevel)] {
+		return errors.New("invalid log level")
+	}
+	
+	return nil
+}
+
+func (c *Config) String() string {
+	data, _ := json.MarshalIndent(c, "", "  ")
+	return string(data)
+}
