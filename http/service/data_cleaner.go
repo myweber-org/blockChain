75cfb48cd62534321738f4c832c41a5b7535e818
ElaraWrites <elara.writes@outlook.com>
2026-02-07 00:34:49 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- http/service/data_cleaner.go
+++ http/service/data_cleaner.go
@@ -238,4 +238,18 @@ func RemoveDuplicates(input []string) []string {
     }
     
     return result
+}package utils
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+
+	return result
 }
