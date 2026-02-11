644313312dea129b7068438223e1365489ca3745
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-11 19:21:51 +0000

    Add log rotation functionality with size-based file splitting

--- http/log_rotator.go
+++ http/log_rotator.go
@@ -621,3 +621,131 @@ func main() {
 
     fmt.Println("Log rotation test completed")
 }
+package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sync"
+    "time"
+)
+
+type RotatingLogger struct {
+    mu           sync.Mutex
+    currentFile  *os.File
+    filePath     string
+    maxSize      int64
+    currentSize  int64
+    rotationCount int
+}
+
+func NewRotatingLogger(filePath string, maxSizeMB int) (*RotatingLogger, error) {
+    maxSize := int64(maxSizeMB) * 1024 * 1024
+    
+    file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return nil, err
+    }
+    
+    return &RotatingLogger{
+        currentFile:  file,
+        filePath:     filePath,
+        maxSize:      maxSize,
+        currentSize:  info.Size(),
+        rotationCount: 0,
+    }, nil
+}
+
+func (rl *RotatingLogger) Write(p []byte) (int, error) {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+    
+    if rl.currentSize+int64(len(p)) > rl.maxSize {
+        if err := rl.rotate(); err != nil {
+            return 0, err
+        }
+    }
+    
+    n, err := rl.currentFile.Write(p)
+    if err == nil {
+        rl.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (rl *RotatingLogger) rotate() error {
+    if rl.currentFile != nil {
+        rl.currentFile.Close()
+    }
+    
+    timestamp := time.Now().Format("20060102_150405")
+    backupPath := fmt.Sprintf("%s.%s.%d", rl.filePath, timestamp, rl.rotationCount)
+    rl.rotationCount++
+    
+    if err := os.Rename(rl.filePath, backupPath); err != nil {
+        return err
+    }
+    
+    file, err := os.OpenFile(rl.filePath, os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    
+    rl.currentFile = file
+    rl.currentSize = 0
+    
+    go rl.cleanOldLogs(5)
+    
+    return nil
+}
+
+func (rl *RotatingLogger) cleanOldLogs(keepCount int) {
+    dir := filepath.Dir(rl.filePath)
+    baseName := filepath.Base(rl.filePath)
+    
+    files, err := filepath.Glob(filepath.Join(dir, baseName+".*"))
+    if err != nil {
+        return
+    }
+    
+    if len(files) > keepCount {
+        filesToDelete := files[:len(files)-keepCount]
+        for _, file := range filesToDelete {
+            os.Remove(file)
+        }
+    }
+}
+
+func (rl *RotatingLogger) Close() error {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+    
+    if rl.currentFile != nil {
+        return rl.currentFile.Close()
+    }
+    return nil
+}
+
+func main() {
+    logger, err := NewRotatingLogger("app.log", 10)
+    if err != nil {
+        panic(err)
+    }
+    defer logger.Close()
+    
+    for i := 0; i < 1000; i++ {
+        message := fmt.Sprintf("Log entry %d: %s\n", i, time.Now().Format(time.RFC3339))
+        logger.Write([]byte(message))
+        time.Sleep(10 * time.Millisecond)
+    }
+    
+    fmt.Println("Log rotation test completed")
+}
