eab9a2389e86100df2324f9aa07fc3fa7c9b1ca3
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-12 00:30:16 +0000

    Add configuration loader with environment variable support

--- http/config_loader.go
+++ http/config_loader.go
@@ -2,79 +2,37 @@ package config
 
 import (
 	"os"
-	"path/filepath"
-
-	"gopkg.in/yaml.v3"
+	"strconv"
 )
 
 type Config struct {
-	Server struct {
-		Host string `yaml:"host" env:"SERVER_HOST"`
-		Port int    `yaml:"port" env:"SERVER_PORT"`
-	} `yaml:"server"`
-	Database struct {
-		Host     string `yaml:"host" env:"DB_HOST"`
-		Port     int    `yaml:"port" env:"DB_PORT"`
-		Name     string `yaml:"name" env:"DB_NAME"`
-		User     string `yaml:"user" env:"DB_USER"`
-		Password string `yaml:"password" env:"DB_PASSWORD"`
-		SSLMode  string `yaml:"ssl_mode" env:"DB_SSL_MODE"`
-	} `yaml:"database"`
-	Logging struct {
-		Level  string `yaml:"level" env:"LOG_LEVEL"`
-		Format string `yaml:"format" env:"LOG_FORMAT"`
-	} `yaml:"logging"`
+	ServerPort int
+	DebugMode  bool
+	LogLevel   string
 }
 
-func LoadConfig(configPath string) (*Config, error) {
-	absPath, err := filepath.Abs(configPath)
-	if err != nil {
-		return nil, err
+func Load() (*Config, error) {
+	cfg := &Config{
+		ServerPort: 8080,
+		DebugMode:  false,
+		LogLevel:   "info",
 	}
 
-	data, err := os.ReadFile(absPath)
-	if err != nil {
-		return nil, err
-	}
-
-	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		return nil, err
-	}
-
-	overrideFromEnv(&cfg)
-
-	return &cfg, nil
-}
-
-func overrideFromEnv(cfg *Config) {
-	cfg.Server.Host = getEnvOrDefault("SERVER_HOST", cfg.Server.Host)
-	cfg.Server.Port = getEnvIntOrDefault("SERVER_PORT", cfg.Server.Port)
-
-	cfg.Database.Host = getEnvOrDefault("DB_HOST", cfg.Database.Host)
-	cfg.Database.Port = getEnvIntOrDefault("DB_PORT", cfg.Database.Port)
-	cfg.Database.Name = getEnvOrDefault("DB_NAME", cfg.Database.Name)
-	cfg.Database.User = getEnvOrDefault("DB_USER", cfg.Database.User)
-	cfg.Database.Password = getEnvOrDefault("DB_PASSWORD", cfg.Database.Password)
-	cfg.Database.SSLMode = getEnvOrDefault("DB_SSL_MODE", cfg.Database.SSLMode)
-
-	cfg.Logging.Level = getEnvOrDefault("LOG_LEVEL", cfg.Logging.Level)
-	cfg.Logging.Format = getEnvOrDefault("LOG_FORMAT", cfg.Logging.Format)
-}
-
-func getEnvOrDefault(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
-	}
-	return defaultValue
-}
-
-func getEnvIntOrDefault(key string, defaultValue int) int {
-	if value := os.Getenv(key); value != "" {
-		var result int
-		if _, err := fmt.Sscanf(value, "%d", &result); err == nil {
-			return result
+	if portStr := os.Getenv("SERVER_PORT"); portStr != "" {
+		if port, err := strconv.Atoi(portStr); err == nil {
+			cfg.ServerPort = port
 		}
 	}
-	return defaultValue
+
+	if debugStr := os.Getenv("DEBUG_MODE"); debugStr != "" {
+		if debug, err := strconv.ParseBool(debugStr); err == nil {
+			cfg.DebugMode = debug
+		}
+	}
+
+	if logLevel := os.Getenv("LOG_LEVEL"); logLevel != "" {
+		cfg.LogLevel = logLevel
+	}
+
+	return cfg, nil
 }
