823787f21e92b93d7ca7c1abca7ee7285e362746
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-12 22:58:50 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- chain/data_cleaner.go
+++ chain/data_cleaner.go
@@ -217,3 +217,72 @@ func main() {
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func removeDuplicates(inputPath, outputPath string) error {
+	inputFile, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	reader := csv.NewReader(inputFile)
+	header, err := reader.Read()
+	if err != nil {
+		return err
+	}
+
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	writer := csv.NewWriter(outputFile)
+	defer writer.Flush()
+
+	if err := writer.Write(header); err != nil {
+		return err
+	}
+
+	seen := make(map[string]bool)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		key := fmt.Sprintf("%v", record)
+		if !seen[key] {
+			seen[key] = true
+			if err := writer.Write(record); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	err := removeDuplicates(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Duplicate removal completed successfully")
+}
