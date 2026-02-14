7610cba3dff8631a24748fe57cb3cacfcd83641e
VioletInquiry <violet.inquiry@outlook.com>
2026-02-14 22:53:47 +0000

    Add log rotation with size-based file splitting and compression

--- http/service/log_rotator.go
+++ http/service/log_rotator.go
@@ -712,3 +712,130 @@ func main() {
 
     fmt.Println("Log rotation demo completed")
 }
+package main
+
+import (
+    "compress/gzip"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sync"
+    "time"
+)
+
+const (
+    maxFileSize = 10 * 1024 * 1024 // 10MB
+    maxBackups  = 5
+)
+
+type RotatingLogger struct {
+    filename   string
+    current    *os.File
+    size       int64
+    mu         sync.Mutex
+}
+
+func NewRotatingLogger(filename string) (*RotatingLogger, error) {
+    rl := &RotatingLogger{filename: filename}
+    if err := rl.openFile(); err != nil {
+        return nil, err
+    }
+    return rl, nil
+}
+
+func (rl *RotatingLogger) Write(p []byte) (int, error) {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+
+    if rl.size+int64(len(p)) > maxFileSize {
+        if err := rl.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := rl.current.Write(p)
+    if err == nil {
+        rl.size += int64(n)
+    }
+    return n, err
+}
+
+func (rl *RotatingLogger) rotate() error {
+    if rl.current != nil {
+        rl.current.Close()
+    }
+
+    timestamp := time.Now().Format("20060102-150405")
+    backupName := fmt.Sprintf("%s.%s.gz", rl.filename, timestamp)
+
+    if err := compressFile(rl.filename, backupName); err != nil {
+        return err
+    }
+
+    cleanupOldBackups(rl.filename)
+
+    return rl.openFile()
+}
+
+func compressFile(src, dst string) error {
+    in, err := os.Open(src)
+    if err != nil {
+        return err
+    }
+    defer in.Close()
+
+    out, err := os.Create(dst)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+
+    gz := gzip.NewWriter(out)
+    defer gz.Close()
+
+    _, err = io.Copy(gz, in)
+    return err
+}
+
+func cleanupOldBackups(baseName string) {
+    pattern := baseName + ".*.gz"
+    matches, err := filepath.Glob(pattern)
+    if err != nil {
+        return
+    }
+
+    if len(matches) > maxBackups {
+        toDelete := matches[:len(matches)-maxBackups]
+        for _, f := range toDelete {
+            os.Remove(f)
+        }
+    }
+}
+
+func (rl *RotatingLogger) openFile() error {
+    f, err := os.OpenFile(rl.filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        return err
+    }
+
+    info, err := f.Stat()
+    if err != nil {
+        f.Close()
+        return err
+    }
+
+    rl.current = f
+    rl.size = info.Size()
+    return nil
+}
+
+func (rl *RotatingLogger) Close() error {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+
+    if rl.current != nil {
+        return rl.current.Close()
+    }
+    return nil
+}
