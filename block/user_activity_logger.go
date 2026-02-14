03fa1f276d473174826fb7dc3c440c00c682c4a7
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-14 21:38:03 +0000

    Add user activity logging middleware for tracking API requests

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -261,3 +261,69 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		duration,
 	)
 }
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp  time.Time
+	Method     string
+	Path       string
+	RemoteAddr string
+	UserAgent  string
+	StatusCode int
+	Duration   time.Duration
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		activity := ActivityLog{
+			Timestamp:  start,
+			Method:     r.Method,
+			Path:       r.URL.Path,
+			RemoteAddr: r.RemoteAddr,
+			UserAgent:  r.UserAgent(),
+			StatusCode: recorder.statusCode,
+			Duration:   duration,
+		}
+		
+		logActivity(activity)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
+}
+
+func logActivity(activity ActivityLog) {
+	log.Printf("[ACTIVITY] %s | %s %s | %d | %s | %s | %v",
+		activity.Timestamp.Format("2006-01-02 15:04:05"),
+		activity.Method,
+		activity.Path,
+		activity.StatusCode,
+		activity.RemoteAddr,
+		activity.UserAgent,
+		activity.Duration,
+	)
+}
