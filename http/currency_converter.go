7e5d8c292c42b1c8206999ef993f47ec866490ba
InkspillVerse <inkspill.verse@outlook.com>
2026-02-16 23:10:10 +0000

    Add currency conversion utility with USD to EUR rate

--- http/currency_converter.go
+++ http/currency_converter.go
@@ -68,3 +68,19 @@ func main() {
 	}
 	fmt.Printf("%.2f EUR = %.2f CAD\n", 50.0, result2)
 }
+package main
+
+import (
+	"fmt"
+)
+
+func ConvertUSDToEUR(amount float64) float64 {
+	const exchangeRate = 0.85
+	return amount * exchangeRate
+}
+
+func main() {
+	usdAmount := 100.0
+	eurAmount := ConvertUSDToEUR(usdAmount)
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}
