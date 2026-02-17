4660320e08560267c4a7a9eda115812263e5bb22
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-17 19:12:17 +0000

    Add utility to remove temporary files older than 7 days

--- chain/file_cleaner.go
+++ chain/file_cleaner.go
@@ -2,157 +2,30 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
 	"time"
 )
 
-const (
-	retentionDays = 30
-	logExtension  = ".log"
-)
-
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: file_cleaner <directory>")
-		os.Exit(1)
-	}
-
-	dir := os.Args[1]
-	err := cleanOldLogs(dir)
-	if err != nil {
-		fmt.Printf("Error cleaning logs: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Log cleanup completed successfully")
-}
-
-func cleanOldLogs(dir string) error {
-	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
-
-	return filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		if filepath.Ext(path) != logExtension {
-			return nil
-		}
-
-		info, err := d.Info()
-		if err != nil {
-			return err
-		}
-
-		if info.ModTime().Before(cutoffTime) {
-			fmt.Printf("Removing old log file: %s (modified: %v)\n", path, info.ModTime())
-			return os.Remove(path)
-		}
-
-		return nil
-	})
-}package main
-
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"time"
-)
+const retentionDays = 7
 
 func main() {
 	tempDir := os.TempDir()
-	cutoff := time.Now().AddDate(0, 0, -7)
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
+
 	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return nil
 		}
-		if info.ModTime().Before(cutoff) && !info.IsDir() {
+
+		if info.ModTime().Before(cutoffTime) {
 			fmt.Printf("Removing old file: %s\n", path)
 			os.Remove(path)
 		}
 		return nil
 	})
+
 	if err != nil {
-		fmt.Printf("Error cleaning temp files: %v\n", err)
+		fmt.Printf("Error cleaning temp directory: %v\n", err)
 	}
-}
-package main
-
-import (
-    "os"
-    "path/filepath"
-    "time"
-)
-
-func main() {
-    tempDir := os.TempDir()
-    cutoff := time.Now().AddDate(0, 0, -7)
-
-    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return nil
-        }
-        if info.IsDir() {
-            return nil
-        }
-        if info.ModTime().Before(cutoff) {
-            os.Remove(path)
-        }
-        return nil
-    })
-}package main
-
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"time"
-)
-
-const (
-	tempDir      = "/tmp/app_temp"
-	daysToKeep   = 7
-)
-
-func main() {
-	err := cleanOldFiles(tempDir, daysToKeep)
-	if err != nil {
-		fmt.Printf("Error cleaning files: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Println("Cleanup completed successfully")
-}
-
-func cleanOldFiles(dirPath string, days int) error {
-	files, err := ioutil.ReadDir(dirPath)
-	if err != nil {
-		return fmt.Errorf("failed to read directory: %w", err)
-	}
-
-	cutoffTime := time.Now().AddDate(0, 0, -days)
-	removedCount := 0
-
-	for _, file := range files {
-		if file.ModTime().Before(cutoffTime) {
-			fullPath := filepath.Join(dirPath, file.Name())
-			err := os.Remove(fullPath)
-			if err != nil {
-				fmt.Printf("Warning: Could not remove %s: %v\n", fullPath, err)
-				continue
-			}
-			removedCount++
-			fmt.Printf("Removed: %s (modified: %v)\n", file.Name(), file.ModTime())
-		}
-	}
-
-	fmt.Printf("Total files removed: %d\n", removedCount)
-	return nil
 }
