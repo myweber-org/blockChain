0210e19941d1353edddec9a5d3b0523b33f632a5
heyQuim <hey.quim@hotmail.com>
2026-02-17 22:55:55 +0000

    Add utility to remove temporary files older than 7 days

--- LevelDB/file_cleaner.go
+++ LevelDB/file_cleaner.go
@@ -221,4 +221,37 @@ func isTempFile(filename string) bool {
 func isFileOld(modTime time.Time) bool {
 	age := time.Since(modTime)
 	return age > maxAgeHours*time.Hour
+}package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().AddDate(0, 0, -7)
+	var removedCount int
+
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if info.ModTime().Before(cutoffTime) {
+			if err := os.RemoveAll(path); err == nil {
+				removedCount++
+				fmt.Printf("Removed: %s\n", path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Cleanup completed. Removed %d items.\n", removedCount)
 }
