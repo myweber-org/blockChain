b0272282a590803be2e9bdae27427bca51ebadbe
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-17 18:27:15 +0000

    Add user activity logging middleware to track API usage

--- LevelDB/user_activity_logger.go
+++ LevelDB/user_activity_logger.go
@@ -1,99 +1,4 @@
-package main
-
-import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
-    "time"
-)
-
-type ActivityLog struct {
-    Timestamp time.Time `json:"timestamp"`
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Details   string    `json:"details,omitempty"`
-}
-
-func logActivity(userID, action, details string) {
-    logEntry := ActivityLog{
-        Timestamp: time.Now(),
-        UserID:    userID,
-        Action:    action,
-        Details:   details,
-    }
-
-    logData, err := json.MarshalIndent(logEntry, "", "  ")
-    if err != nil {
-        log.Printf("Failed to marshal log entry: %v", err)
-        return
-    }
-
-    fmt.Println(string(logData))
-
-    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        log.Printf("Failed to open log file: %v", err)
-        return
-    }
-    defer file.Close()
-
-    if _, err := file.Write(append(logData, '\n')); err != nil {
-        log.Printf("Failed to write to log file: %v", err)
-    }
-}
-
-func main() {
-    logActivity("user123", "LOGIN", "User logged in from IP 192.168.1.100")
-    logActivity("user456", "LOGOUT", "Session duration: 1h 30m")
-    logActivity("user789", "UPDATE_PROFILE", "Changed email address")
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
-	
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
-	duration := time.Since(startTime)
-	
-	log.Printf(
-		"%s %s %d %s %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.RemoteAddr,
-	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
+package middleware
 
 import (
 	"log"
@@ -111,14 +16,14 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+	
 	al.handler.ServeHTTP(w, r)
+	
 	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | RemoteAddr: %s",
+	log.Printf("Activity: %s %s from %s completed in %v",
 		r.Method,
 		r.URL.Path,
-		duration,
 		r.RemoteAddr,
+		duration,
 	)
 }
