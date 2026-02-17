169b806aea57f274ee93594188d4c0376a4d9278
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-17 01:01:04 +0000

    Add utility to remove temporary files older than 7 days

--- http/file_cleaner.go
+++ http/file_cleaner.go
@@ -1,99 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"time"
+    "os"
+    "path/filepath"
+    "time"
 )
 
 func main() {
-	tempDir := os.TempDir()
-	cutoffTime := time.Now().AddDate(0, 0, -7)
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-		if !info.Mode().IsRegular() {
-			return nil
-		}
-		if info.ModTime().Before(cutoffTime) {
-			fmt.Printf("Removing old file: %s\n", path)
-			os.Remove(path)
-		}
-		return nil
-	})
-	if err != nil {
-		fmt.Printf("Error cleaning temp directory: %v\n", err)
-	}
-}package main
+    tempDir := os.TempDir()
+    cutoff := time.Now().AddDate(0, 0, -7)
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"time"
-)
-
-const daysToKeep = 7
-
-func main() {
-	tempDir := os.TempDir()
-	cutoffTime := time.Now().AddDate(0, 0, -daysToKeep)
-
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		if info.ModTime().Before(cutoffTime) {
-			fmt.Printf("Removing old file: %s\n", path)
-			os.Remove(path)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		fmt.Printf("Error cleaning temp directory: %v\n", err)
-	}
-}package main
-
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"time"
-)
-
-func main() {
-	tempDir := os.TempDir()
-	cutoff := time.Now().AddDate(0, 0, -7)
-	var removedCount int
-
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-		if info.IsDir() {
-			return nil
-		}
-		if info.ModTime().Before(cutoff) {
-			if err := os.Remove(path); err == nil {
-				removedCount++
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		fmt.Printf("Error walking directory: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Cleaned %d temporary files older than 7 days\n", removedCount)
+    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            os.Remove(path)
+        }
+        return nil
+    })
 }
