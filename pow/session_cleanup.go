6567a84ea519b916a270e1a10ad8c50dcedf72de
ElaraWrites <elara.writes@outlook.com>
2026-02-18 15:50:24 +0000

    Add session cleanup cron job to remove expired sessions daily

--- pow/session_cleanup.go
+++ pow/session_cleanup.go
@@ -161,4 +161,52 @@ func cleanupExpiredSessions(db *database.DB) {
 
 	rowsAffected, _ := result.RowsAffected()
 	log.Printf("Cleaned up %d expired sessions", rowsAffected)
+}package main
+
+import (
+    "context"
+    "log"
+    "time"
+
+    "github.com/yourproject/database"
+)
+
+const cleanupInterval = 24 * time.Hour
+const sessionTTL = 30 * 24 * time.Hour
+
+func cleanupExpiredSessions(ctx context.Context) error {
+    db := database.GetConnection()
+    cutoff := time.Now().Add(-sessionTTL)
+
+    result, err := db.ExecContext(ctx,
+        "DELETE FROM user_sessions WHERE last_activity < ?",
+        cutoff)
+    if err != nil {
+        return err
+    }
+
+    rows, _ := result.RowsAffected()
+    log.Printf("Cleaned up %d expired sessions", rows)
+    return nil
+}
+
+func startCleanupScheduler() {
+    ticker := time.NewTicker(cleanupInterval)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ticker.C:
+            ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+            if err := cleanupExpiredSessions(ctx); err != nil {
+                log.Printf("Session cleanup failed: %v", err)
+            }
+            cancel()
+        }
+    }
+}
+
+func main() {
+    log.Println("Starting session cleanup scheduler...")
+    startCleanupScheduler()
 }
