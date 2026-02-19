772afe46e18995ff8bb630435ba87003d59ebf2d
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-19 19:38:59 +0000

    Add file encryption utility with AES-256-CBC

--- pow/file_encryptor.go
+++ pow/file_encryptor.go
@@ -308,4 +308,88 @@ func main() {
 		fmt.Println("Invalid action. Use 'encrypt' or 'decrypt'")
 		os.Exit(1)
 	}
+}package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
+    "errors"
+    "io"
+    "os"
+)
+
+func encryptData(plaintext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+    iv := ciphertext[:aes.BlockSize]
+    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+        return nil, err
+    }
+
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+    return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    if len(ciphertext) < aes.BlockSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    iv := ciphertext[:aes.BlockSize]
+    ciphertext = ciphertext[aes.BlockSize:]
+
+    stream := cipher.NewCFBDecrypter(block, iv)
+    stream.XORKeyStream(ciphertext, ciphertext)
+
+    return ciphertext, nil
+}
+
+func encodeBase64(data []byte) string {
+    return base64.StdEncoding.EncodeToString(data)
+}
+
+func decodeBase64(s string) ([]byte, error) {
+    return base64.StdEncoding.DecodeString(s)
+}
+
+func main() {
+    key := make([]byte, 32)
+    if _, err := io.ReadFull(rand.Reader, key); err != nil {
+        panic(err)
+    }
+
+    secretMessage := []byte("This is a confidential message")
+    
+    encrypted, err := encryptData(secretMessage, key)
+    if err != nil {
+        panic(err)
+    }
+
+    encoded := encodeBase64(encrypted)
+    fmt.Println("Encrypted (base64):", encoded)
+
+    decoded, err := decodeBase64(encoded)
+    if err != nil {
+        panic(err)
+    }
+
+    decrypted, err := decryptData(decoded, key)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Decrypted:", string(decrypted))
 }
