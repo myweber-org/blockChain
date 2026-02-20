1a14b5a024acad9ca43338c1dc1da8b2bb6bb389
heyQuim <hey.quim@hotmail.com>
2026-02-20 22:43:27 +0000

    Add file processing utility with concurrency support

--- chain/file_processor.go
+++ chain/file_processor.go
@@ -434,4 +434,106 @@ func main() {
 	fmt.Printf("  Total words: %d\n", totalWords)
 	fmt.Printf("  Total characters: %d\n", totalChars)
 	fmt.Printf("  Processing time: %v\n", duration)
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type FileProcessor struct {
+	mu       sync.Mutex
+	fileList []string
+}
+
+func NewFileProcessor() *FileProcessor {
+	return &FileProcessor{
+		fileList: make([]string, 0),
+	}
+}
+
+func (fp *FileProcessor) ScanDirectory(dirPath string) error {
+	return filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			fp.mu.Lock()
+			fp.fileList = append(fp.fileList, path)
+			fp.mu.Unlock()
+		}
+		return nil
+	})
+}
+
+func (fp *FileProcessor) ProcessFiles() []string {
+	var wg sync.WaitGroup
+	results := make([]string, 0, len(fp.fileList))
+	resultChan := make(chan string, len(fp.fileList))
+
+	for _, file := range fp.fileList {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			result := fp.processSingleFile(f)
+			resultChan <- result
+		}(file)
+	}
+
+	wg.Wait()
+	close(resultChan)
+
+	for result := range resultChan {
+		results = append(results, result)
+	}
+
+	return results
+}
+
+func (fp *FileProcessor) processSingleFile(filePath string) string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Sprintf("ERROR: %s", err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineCount := 0
+	for scanner.Scan() {
+		lineCount++
+	}
+
+	return fmt.Sprintf("Processed %s: %d lines", filepath.Base(filePath), lineCount)
+}
+
+func (fp *FileProcessor) GetFileCount() int {
+	fp.mu.Lock()
+	defer fp.mu.Unlock()
+	return len(fp.fileList)
+}
+
+func main() {
+	processor := NewFileProcessor()
+	
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_processor <directory>")
+		os.Exit(1)
+	}
+
+	dirPath := os.Args[1]
+	err := processor.ScanDirectory(dirPath)
+	if err != nil {
+		fmt.Printf("Error scanning directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Found %d files\n", processor.GetFileCount())
+	results := processor.ProcessFiles()
+	
+	for _, result := range results {
+		fmt.Println(result)
+	}
 }
