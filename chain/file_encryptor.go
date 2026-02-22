760cf44ba33fd830628536957fe936724ab0a8ac
ElaraWrites <elara.writes@outlook.com>
2026-02-22 15:48:54 +0000

    Add file encryption utility with AES-256-GCM and key derivation

--- chain/file_encryptor.go
+++ chain/file_encryptor.go
@@ -512,3 +512,122 @@ func main() {
 	}
 	fmt.Printf("Decrypted: %s\n", decrypted)
 }
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptFile(inputPath, outputPath, passphrase string) error {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return err
+	}
+
+	key := deriveKey(passphrase, salt)
+
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
+
+	outputData := append(salt, nonce...)
+	outputData = append(outputData, ciphertext...)
+
+	return os.WriteFile(outputPath, outputData, 0644)
+}
+
+func decryptFile(inputPath, outputPath, passphrase string) error {
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	if len(ciphertext) < 48 {
+		return errors.New("file too short to be valid")
+	}
+
+	salt := ciphertext[:16]
+	nonce := ciphertext[16:32]
+	actualCiphertext := ciphertext[32:]
+
+	key := deriveKey(passphrase, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	plaintext, err := gcm.Open(nil, nonce, actualCiphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: file_encryptor <encrypt|decrypt> <input> <output> <passphrase>")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
+	passphrase := os.Args[4]
+
+	var err error
+	switch operation {
+	case "encrypt":
+		err = encryptFile(inputPath, outputPath, passphrase)
+	case "decrypt":
+		err = decryptFile(inputPath, outputPath, passphrase)
+	default:
+		err = errors.New("invalid operation. Use 'encrypt' or 'decrypt'")
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Operation %s completed successfully\n", operation)
+}
