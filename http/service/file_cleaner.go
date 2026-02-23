2ee3718cd644c25fe40e032fb8d9110c93b44bb8
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-23 23:29:45 +0000

    Add utility to remove temporary files older than 7 days

--- http/service/file_cleaner.go
+++ http/service/file_cleaner.go
@@ -46,4 +46,37 @@ func main() {
         }
         return nil
     })
+}package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().AddDate(0, 0, -7)
+	var removedCount int
+
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if info.ModTime().Before(cutoffTime) && !info.IsDir() {
+			if err := os.Remove(path); err == nil {
+				removedCount++
+				fmt.Printf("Removed: %s\n", path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Cleanup completed. Removed %d files.\n", removedCount)
 }
