ca87d03dc890c9f901ff64567e999ac0872790d9
heyQuim <hey.quim@hotmail.com>
2026-02-25 15:47:28 +0000

    Implement log rotation with size-based file splitting and compression

--- http/service/log_rotator.go
+++ http/service/log_rotator.go
@@ -136,3 +136,183 @@ func main() {
 		time.Sleep(10 * time.Millisecond)
 	}
 }
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+const (
+	maxFileSize = 10 * 1024 * 1024 // 10MB
+	backupCount = 5
+)
+
+type RotatingLogger struct {
+	mu         sync.Mutex
+	file       *os.File
+	size       int64
+	basePath   string
+	currentDay string
+}
+
+func NewRotatingLogger(path string) (*RotatingLogger, error) {
+	rl := &RotatingLogger{
+		basePath: path,
+	}
+	if err := rl.rotateIfNeeded(); err != nil {
+		return nil, err
+	}
+	return rl, nil
+}
+
+func (rl *RotatingLogger) Write(p []byte) (int, error) {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if err := rl.rotateIfNeeded(); err != nil {
+		return 0, err
+	}
+
+	n, err := rl.file.Write(p)
+	rl.size += int64(n)
+	return n, err
+}
+
+func (rl *RotatingLogger) rotateIfNeeded() error {
+	today := time.Now().Format("2006-01-02")
+	if rl.currentDay != today {
+		if err := rl.rotateByDate(); err != nil {
+			return err
+		}
+		rl.currentDay = today
+	}
+
+	if rl.size >= maxFileSize {
+		return rl.rotateBySize()
+	}
+
+	if rl.file == nil {
+		return rl.openCurrentFile()
+	}
+	return nil
+}
+
+func (rl *RotatingLogger) rotateByDate() error {
+	if rl.file != nil {
+		rl.file.Close()
+		rl.file = nil
+	}
+	return rl.openCurrentFile()
+}
+
+func (rl *RotatingLogger) rotateBySize() error {
+	if rl.file != nil {
+		rl.file.Close()
+		if err := rl.compressCurrentFile(); err != nil {
+			log.Printf("Failed to compress log file: %v", err)
+		}
+		rl.cleanupOldFiles()
+		rl.file = nil
+		rl.size = 0
+	}
+	return rl.openCurrentFile()
+}
+
+func (rl *RotatingLogger) openCurrentFile() error {
+	dir := filepath.Dir(rl.basePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	today := time.Now().Format("2006-01-02")
+	path := fmt.Sprintf("%s-%s.log", rl.basePath, today)
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	rl.file = file
+	rl.size = info.Size()
+	rl.currentDay = today
+	return nil
+}
+
+func (rl *RotatingLogger) compressCurrentFile() error {
+	today := time.Now().Format("2006-01-02")
+	sourcePath := fmt.Sprintf("%s-%s.log", rl.basePath, today)
+	destPath := fmt.Sprintf("%s-%s.log.gz", rl.basePath, today)
+
+	sourceFile, err := os.Open(sourcePath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(destPath)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	gzWriter := gzip.NewWriter(destFile)
+	defer gzWriter.Close()
+
+	_, err = io.Copy(gzWriter, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	return os.Remove(sourcePath)
+}
+
+func (rl *RotatingLogger) cleanupOldFiles() {
+	files, err := filepath.Glob(rl.basePath + "-*.log.gz")
+	if err != nil {
+		return
+	}
+
+	if len(files) > backupCount {
+		for i := 0; i < len(files)-backupCount; i++ {
+			os.Remove(files[i])
+		}
+	}
+}
+
+func (rl *RotatingLogger) Close() error {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if rl.file != nil {
+		return rl.file.Close()
+	}
+	return nil
+}
+
+func main() {
+	logger, err := NewRotatingLogger("/var/log/myapp/application")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	customLog := log.New(logger, "", log.LstdFlags)
+
+	for i := 0; i < 100; i++ {
+		customLog.Printf("Log entry number %d", i)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
