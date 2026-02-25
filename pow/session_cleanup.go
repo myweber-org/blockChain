e77b545d84ecd9a0a38db68e46c9420c4214d8d0
FelixMakes <felix.makes@hotmail.com>
2026-02-25 00:09:16 +0000

    Add session cleanup job to remove expired sessions daily

--- pow/session_cleanup.go
+++ pow/session_cleanup.go
@@ -210,3 +210,53 @@ func main() {
     log.Println("Starting session cleanup scheduler...")
     startCleanupScheduler()
 }
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type Session struct {
+	ID        string
+	UserID    string
+	ExpiresAt time.Time
+}
+
+type SessionStore struct {
+	sessions map[string]Session
+}
+
+func NewSessionStore() *SessionStore {
+	return &SessionStore{
+		sessions: make(map[string]Session),
+	}
+}
+
+func (s *SessionStore) CleanExpiredSessions() {
+	now := time.Now()
+	count := 0
+	for id, session := range s.sessions {
+		if session.ExpiresAt.Before(now) {
+			delete(s.sessions, id)
+			count++
+		}
+	}
+	log.Printf("Cleaned %d expired sessions", count)
+}
+
+func startCleanupJob(store *SessionStore) {
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		store.CleanExpiredSessions()
+	}
+}
+
+func main() {
+	store := NewSessionStore()
+	go startCleanupJob(store)
+
+	select {}
+}
