a5bbb2eed89a9819db0b09e3d95bf391c2a28c22
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-26 16:57:57 +0000

    Add configuration loader with YAML support and environment variable overrides

--- block/config_loader.go
+++ block/config_loader.go
@@ -52,4 +52,87 @@ func getEnvAsSlice(key string, defaultValue []string, sep string) []string {
         return defaultValue
     }
     return strings.Split(valueStr, sep)
+}package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Port    string `yaml:"port" env:"SERVER_PORT"`
+		Timeout int    `yaml:"timeout" env:"SERVER_TIMEOUT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     string `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	overrideWithEnvVars(config)
+
+	return config, nil
+}
+
+func overrideWithEnvVars(config *Config) {
+	overrideStruct(config.Server)
+	overrideStruct(config.Database)
+	overrideStruct(config.Logging)
+}
+
+func overrideStruct(s interface{}) {
+	val := reflect.ValueOf(s).Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+
+		envTag := fieldType.Tag.Get("env")
+		if envTag == "" {
+			continue
+		}
+
+		envValue := os.Getenv(envTag)
+		if envValue == "" {
+			continue
+		}
+
+		switch field.Kind() {
+		case reflect.String:
+			field.SetString(envValue)
+		case reflect.Int:
+			if intValue, err := strconv.Atoi(envValue); err == nil {
+				field.SetInt(int64(intValue))
+			}
+		case reflect.Bool:
+			if boolValue, err := strconv.ParseBool(strings.ToLower(envValue)); err == nil {
+				field.SetBool(boolValue)
+			}
+		}
+	}
 }
