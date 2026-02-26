81030e822c76bee3dddb059b8ec7a88bbf6db761
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-26 22:55:44 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- http/data_cleaner.go
+++ http/data_cleaner.go
@@ -179,3 +179,26 @@ func main() {
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	input := []int{1, 2, 2, 3, 4, 4, 5, 6, 6}
+	cleaned := RemoveDuplicates(input)
+	fmt.Printf("Original: %v\n", input)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
