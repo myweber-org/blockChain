0cf33e04959cf531952f97da8aadd1ce0da19b33
heyQuim <hey.quim@hotmail.com>
2026-02-26 16:12:34 +0000

    Implement log rotation with size-based and time-based triggers

--- LevelDB/log_rotator.go
+++ LevelDB/log_rotator.go
@@ -292,4 +292,135 @@ func main() {
 		logger.Write([]byte(msg))
 		time.Sleep(100 * time.Millisecond)
 	}
+}package main
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+    "sync"
+    "time"
+)
+
+type Rotator struct {
+    filePath    string
+    maxSize     int64
+    maxAge      time.Duration
+    currentFile *os.File
+    currentSize int64
+    mu          sync.Mutex
+}
+
+func NewRotator(filePath string, maxSize int64, maxAge time.Duration) (*Rotator, error) {
+    r := &Rotator{
+        filePath: filePath,
+        maxSize:  maxSize,
+        maxAge:   maxAge,
+    }
+    if err := r.openCurrent(); err != nil {
+        return nil, err
+    }
+    go r.ageMonitor()
+    return r, nil
+}
+
+func (r *Rotator) openCurrent() error {
+    dir := filepath.Dir(r.filePath)
+    if err := os.MkdirAll(dir, 0755); err != nil {
+        return err
+    }
+    f, err := os.OpenFile(r.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    stat, err := f.Stat()
+    if err != nil {
+        f.Close()
+        return err
+    }
+    r.currentFile = f
+    r.currentSize = stat.Size()
+    return nil
+}
+
+func (r *Rotator) rotate() error {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+
+    if r.currentFile != nil {
+        r.currentFile.Close()
+    }
+
+    timestamp := time.Now().Format("20060102_150405")
+    rotatedPath := fmt.Sprintf("%s.%s", r.filePath, timestamp)
+    if err := os.Rename(r.filePath, rotatedPath); err != nil {
+        return err
+    }
+
+    oldFiles, err := filepath.Glob(r.filePath + ".*")
+    if err == nil && r.maxAge > 0 {
+        cutoff := time.Now().Add(-r.maxAge)
+        for _, oldFile := range oldFiles {
+            if info, err := os.Stat(oldFile); err == nil && info.ModTime().Before(cutoff) {
+                os.Remove(oldFile)
+            }
+        }
+    }
+
+    return r.openCurrent()
+}
+
+func (r *Rotator) Write(p []byte) (int, error) {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+
+    if r.currentSize+int64(len(p)) > r.maxSize && r.maxSize > 0 {
+        if err := r.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := r.currentFile.Write(p)
+    if err == nil {
+        r.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (r *Rotator) ageMonitor() {
+    if r.maxAge <= 0 {
+        return
+    }
+    ticker := time.NewTicker(time.Hour)
+    defer ticker.Stop()
+    for range ticker.C {
+        if info, err := os.Stat(r.filePath); err == nil {
+            if time.Since(info.ModTime()) > r.maxAge {
+                r.rotate()
+            }
+        }
+    }
+}
+
+func (r *Rotator) Close() error {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+    if r.currentFile != nil {
+        return r.currentFile.Close()
+    }
+    return nil
+}
+
+func main() {
+    rotator, err := NewRotator("/var/log/myapp/app.log", 10*1024*1024, 7*24*time.Hour)
+    if err != nil {
+        panic(err)
+    }
+    defer rotator.Close()
+
+    for i := 0; i < 100; i++ {
+        msg := fmt.Sprintf("Log entry %d at %s\n", i, time.Now().Format(time.RFC3339))
+        rotator.Write([]byte(msg))
+        time.Sleep(100 * time.Millisecond)
+    }
 }
