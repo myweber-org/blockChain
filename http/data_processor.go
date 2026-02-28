34a4ad2bf44e1d0ccea55bb7b7785fd84ed583ef
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-28 20:40:11 +0000

    Add data processing utility for cleaning and validating input strings

--- http/data_processor.go
+++ http/data_processor.go
@@ -134,3 +134,42 @@ func main() {
     output := FilterAndDouble(input, 9)
     fmt.Println("Processed slice:", output)
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+	emailRegex      *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+		emailRegex:      regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) NormalizeEmail(email string) string {
+	cleaned := dp.CleanString(email)
+	return strings.ToLower(cleaned)
+}
+
+func (dp *DataProcessor) ProcessUserInput(name, email string) (string, string, bool) {
+	cleanName := dp.CleanString(name)
+	normalizedEmail := dp.NormalizeEmail(email)
+	isValid := dp.ValidateEmail(normalizedEmail)
+	return cleanName, normalizedEmail, isValid
+}
