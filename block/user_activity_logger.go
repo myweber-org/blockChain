fbc1ef2f23abd9f15b80c550f1661e935b6e66c3
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-04 20:51:55 +0000

    Add user activity logging middleware for tracking API usage

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -16,22 +16,20 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
 	recorder := &responseRecorder{
 		ResponseWriter: w,
 		statusCode:     http.StatusOK,
 	}
-	
+
 	al.handler.ServeHTTP(recorder, r)
-	
+
 	duration := time.Since(start)
-	
 	log.Printf(
 		"%s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
 		recorder.statusCode,
-		duration.String(),
+		duration,
 		r.RemoteAddr,
 	)
 }
@@ -44,188 +42,4 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-	al.handler.ServeHTTP(recorder, r)
-	duration := time.Since(start)
-	log.Printf("%s %s %d %v", r.Method, r.URL.Path, recorder.statusCode, duration)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
-		r.Method,
-		r.URL.Path,
-		duration,
-		time.Now().Format(time.RFC3339),
-	)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLog struct {
-	Timestamp time.Time
-	Method    string
-	Path      string
-	UserAgent string
-	IP        string
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		lw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-		next.ServeHTTP(lw, r)
-
-		activity := ActivityLog{
-			Timestamp: start,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			UserAgent: r.UserAgent(),
-			IP:        r.RemoteAddr,
-		}
-
-		log.Printf("ACTIVITY: %s %s %s %s %d %v",
-			activity.IP,
-			activity.Method,
-			activity.Path,
-			activity.UserAgent,
-			lw.statusCode,
-			time.Since(start),
-		)
-	})
-}
-
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	limiter *RateLimiter
-}
-
-type RateLimiter struct {
-	interval time.Duration
-	last     time.Time
-}
-
-func NewActivityLogger(interval time.Duration) *ActivityLogger {
-	return &ActivityLogger{
-		limiter: &RateLimiter{
-			interval: interval,
-			last:     time.Now().Add(-interval),
-		},
-	}
-}
-
-func (rl *RateLimiter) Allow() bool {
-	now := time.Now()
-	if now.Sub(rl.last) >= rl.interval {
-		rl.last = now
-		return true
-	}
-	return false
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if al.limiter.Allow() {
-			log.Printf("Activity: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
-		}
-		next.ServeHTTP(w, r)
-	})
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
 }
