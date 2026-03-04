f729a8a35149618354f82e432daea8e2d7ec426d
VioletInquiry <violet.inquiry@outlook.com>
2026-03-04 02:16:11 +0000

    Add user activity logging with timestamps and event types

--- http/service/user_activity_logger.go
+++ http/service/user_activity_logger.go
@@ -333,4 +333,70 @@ func main() {
             fmt.Printf("Logged: %s - %s\n", activity.UserID, activity.Action)
         }
     }
+}package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityEvent struct {
+	Timestamp time.Time
+	UserID    string
+	EventType string
+	Details   string
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, eventType, details string) {
+	event := ActivityEvent{
+		Timestamp: time.Now(),
+		UserID:    userID,
+		EventType: eventType,
+		Details:   details,
+	}
+
+	logEntry := fmt.Sprintf("%s | User: %s | Event: %s | Details: %s\n",
+		event.Timestamp.Format("2006-01-02 15:04:05"),
+		event.UserID,
+		event.EventType,
+		event.Details)
+
+	if _, err := al.logFile.WriteString(logEntry); err != nil {
+		log.Printf("Failed to write activity log: %v", err)
+	}
+}
+
+func (al *ActivityLogger) Close() {
+	if al.logFile != nil {
+		al.logFile.Close()
+	}
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	logger.LogActivity("user123", "LOGIN", "Successful authentication")
+	logger.LogActivity("user123", "VIEW_PAGE", "Accessed dashboard")
+	logger.LogActivity("user456", "UPDATE_PROFILE", "Changed email address")
+	logger.LogActivity("user123", "LOGOUT", "Session terminated")
+
+	fmt.Println("Activity logging completed. Check activity.log file.")
 }
