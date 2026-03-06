3495246235a0460f45b19e9d78ece1f3f822ca2b
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-06 14:43:31 +0000

    Add user activity logging with timestamps and event types

--- http/user_activity_logger.go
+++ http/user_activity_logger.go
@@ -148,4 +148,77 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package main
+
+import (
+    "encoding/json"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityEvent struct {
+    UserID    string    `json:"user_id"`
+    EventType string    `json:"event_type"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details"`
+}
+
+type ActivityLogger struct {
+    file *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{file: file}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+    event := ActivityEvent{
+        UserID:    userID,
+        EventType: eventType,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    data, err := json.Marshal(event)
+    if err != nil {
+        return err
+    }
+
+    data = append(data, '\n')
+    _, err = l.file.Write(data)
+    return err
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.file.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("activity.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "User logged in from web browser")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    err = logger.LogActivity("user123", "search", "Searched for 'golang tutorials'")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    err = logger.LogActivity("user456", "purchase", "Purchased item ID: 789")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    log.Println("Activity logging completed")
 }
