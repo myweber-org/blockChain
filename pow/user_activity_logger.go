53448ea2af3223b2716ea0868377fc8b6382dc02
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-06 21:01:01 +0000

    Add user activity logging with timestamps and action types

--- pow/user_activity_logger.go
+++ pow/user_activity_logger.go
@@ -240,4 +240,53 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "os"
+    "time"
+)
+
+type ActivityLog struct {
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Details   string    `json:"details,omitempty"`
+}
+
+func logActivity(userID, action, details string) ActivityLog {
+    log := ActivityLog{
+        Timestamp: time.Now(),
+        UserID:    userID,
+        Action:    action,
+        Details:   details,
+    }
+    return log
+}
+
+func saveLogToFile(log ActivityLog, filename string) error {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    encoder := json.NewEncoder(file)
+    encoder.SetIndent("", "  ")
+    return encoder.Encode(log)
+}
+
+func main() {
+    log := logActivity("user123", "LOGIN", "User logged in from IP 192.168.1.100")
+    
+    err := saveLogToFile(log, "activity_log.json")
+    if err != nil {
+        fmt.Printf("Failed to save log: %v\n", err)
+        return
+    }
+    
+    fmt.Printf("Activity logged: %s performed %s at %s\n", 
+        log.UserID, log.Action, log.Timestamp.Format(time.RFC3339))
 }
