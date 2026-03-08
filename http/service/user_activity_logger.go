5700df86f758a0032b8587c4978598915f916e4f
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-08 15:31:50 +0000

    Add user activity logging middleware for tracking API usage

--- http/service/user_activity_logger.go
+++ http/service/user_activity_logger.go
@@ -591,4 +591,45 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	writer := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+	
+	al.handler.ServeHTTP(writer, r)
+	
+	duration := time.Since(start)
+	log.Printf(
+		"%s %s %d %s %s",
+		r.Method,
+		r.URL.Path,
+		writer.statusCode,
+		duration,
+		r.RemoteAddr,
+	)
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
