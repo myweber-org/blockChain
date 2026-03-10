b793e96475b2c5890a1d219e46aeb135b73576e2
VioletInquiry <violet.inquiry@outlook.com>
2026-03-10 01:21:49 +0000

    Add user activity logging with timestamps and session IDs

--- block/user_activity_logger.go
+++ block/user_activity_logger.go
@@ -124,4 +124,63 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	SessionID string
+	UserID    string
+	Action    string
+	Details   map[string]string
+}
+
+func NewActivityLog(sessionID, userID, action string) *ActivityLog {
+	return &ActivityLog{
+		Timestamp: time.Now().UTC(),
+		SessionID: sessionID,
+		UserID:    userID,
+		Action:    action,
+		Details:   make(map[string]string),
+	}
+}
+
+func (al *ActivityLog) AddDetail(key, value string) {
+	al.Details[key] = value
+}
+
+func (al *ActivityLog) WriteToFile(filename string) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	logEntry := fmt.Sprintf("[%s] Session:%s User:%s Action:%s Details:%v\n",
+		al.Timestamp.Format(time.RFC3339),
+		al.SessionID,
+		al.UserID,
+		al.Action,
+		al.Details)
+
+	_, err = file.WriteString(logEntry)
+	return err
+}
+
+func main() {
+	logger := NewActivityLog("sess_abc123", "user_789", "login")
+	logger.AddDetail("ip", "192.168.1.100")
+	logger.AddDetail("user_agent", "Mozilla/5.0")
+
+	if err := logger.WriteToFile("activity.log"); err != nil {
+		log.Fatal("Failed to write log:", err)
+	}
+
+	fmt.Println("Activity logged successfully")
 }
