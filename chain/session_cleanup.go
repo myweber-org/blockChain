8fbb476af9ce945e9ded68439aaaffa0e6fa6341
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-12 16:44:28 +0000

    Add session cleanup job to remove expired sessions from the database

--- chain/session_cleanup.go
+++ chain/session_cleanup.go
@@ -55,4 +55,53 @@ func main() {
             fmt.Printf("Cleanup error: %v\n", err)
         }
     }
+}package main
+
+import (
+    "context"
+    "log"
+    "time"
+
+    "github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+    cleanupInterval = 1 * time.Hour
+    sessionTTL      = 24 * time.Hour
+)
+
+func main() {
+    dbURL := "postgresql://user:pass@localhost:5432/dbname"
+    pool, err := pgxpool.New(context.Background(), dbURL)
+    if err != nil {
+        log.Fatalf("Unable to connect to database: %v", err)
+    }
+    defer pool.Close()
+
+    ticker := time.NewTicker(cleanupInterval)
+    defer ticker.Stop()
+
+    log.Println("Session cleanup job started")
+    for range ticker.C {
+        cleanupExpiredSessions(pool)
+    }
+}
+
+func cleanupExpiredSessions(pool *pgxpool.Pool) {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    cutoff := time.Now().Add(-sessionTTL)
+    query := `DELETE FROM user_sessions WHERE last_activity < $1`
+
+    result, err := pool.Exec(ctx, query, cutoff)
+    if err != nil {
+        log.Printf("Failed to cleanup sessions: %v", err)
+        return
+    }
+
+    rowsAffected := result.RowsAffected()
+    if rowsAffected > 0 {
+        log.Printf("Cleaned up %d expired sessions", rowsAffected)
+    }
 }
