65552799da94279b653498cc372de26bf12db06f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-13 16:11:24 +0000

    Add data processing utility for cleaning and normalizing input strings

--- block/data_processor.go
+++ block/data_processor.go
@@ -652,3 +652,43 @@ func TruncateString(s string, maxLength int) string {
 	}
 	return s[:maxLength]
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	normalized := dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+	return normalized
+}
+
+func (dp *DataProcessor) NormalizeCase(input string, toUpper bool) string {
+	cleaned := dp.CleanString(input)
+	if toUpper {
+		return strings.ToUpper(cleaned)
+	}
+	return strings.ToLower(cleaned)
+}
+
+func (dp *DataProcessor) ExtractAlphanumeric(input string) string {
+	alnumRegex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	return alnumRegex.ReplaceAllString(input, "")
+}
+
+func (dp *DataProcessor) ValidateEmail(input string) bool {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(input)
+}
