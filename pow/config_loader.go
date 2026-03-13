4639ff4c004cba01f1b75d3136a1657b2ae1bcdb
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-13 00:18:20 +0000

    Add configuration loader with YAML support and validation

--- pow/config_loader.go
+++ pow/config_loader.go
@@ -95,4 +95,90 @@ func validateConfig(cfg *Config) error {
 	}
 	
 	return nil
+}package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+}
+
+type ServerConfig struct {
+	Port         int    `yaml:"port"`
+	ReadTimeout  int    `yaml:"read_timeout"`
+	WriteTimeout int    `yaml:"write_timeout"`
+	DebugMode    bool   `yaml:"debug_mode"`
+	LogLevel     string `yaml:"log_level"`
+}
+
+type AppConfig struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+	if filePath == "" {
+		return nil, errors.New("config file path cannot be empty")
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := validateConfig(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func validateConfig(config *AppConfig) error {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+
+	if config.Database.Host == "" {
+		return errors.New("database host cannot be empty")
+	}
+
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		return errors.New("database port must be between 1 and 65535")
+	}
+
+	if config.Server.LogLevel != "" {
+		validLevels := map[string]bool{
+			"debug": true,
+			"info":  true,
+			"warn":  true,
+			"error": true,
+		}
+		if !validLevels[config.Server.LogLevel] {
+			return errors.New("invalid log level specified")
+		}
+	}
+
+	return nil
 }
