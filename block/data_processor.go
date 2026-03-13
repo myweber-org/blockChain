82aa9b266c69bda3d72ff437b46fab036363a565
YourApexArchive <your.apex.archive@hotmail.com>
2026-03-13 19:19:35 +0000

    Add data validation and sanitization functions for user input

--- block/data_processor.go
+++ block/data_processor.go
@@ -692,3 +692,28 @@ func (dp *DataProcessor) ValidateEmail(input string) bool {
 	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 	return emailRegex.MatchString(input)
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func TruncateString(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	return s[:maxLen]
+}
